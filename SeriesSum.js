e0ac4952debc1ba199bbf1af0ae0b6941e48e398
rogercodes1 <roger.ucsb@gmail.com>
2018-08-10 12:29:13 -0700

    added inverseMod

--- SeriesSum.js
+++ SeriesSum.js
@@ -1,7 +1,9 @@
 function SeriesSum(n){
   let sum = 0
   for (let i = 0; i<n;i++){
-
+    sum += 1/(1+i*3)
   }
+  return sum.toFixed(2)
 }
 SeriesSum(2)
+console.log(SeriesSum(2));
---
+++ inverseMod.js
@@ -0,0 +1,8 @@
+let a = 2
+let n = 5
+// let a = 48
+// let n = 101
+let inverseMod = function(a,n) {
+
+}
+inverseMod(a,n)
