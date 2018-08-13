6d5d1ab5c042c737809534e2accf5841568c026d
rogercodes1 <roger.ucsb@gmail.com>
2018-08-13 14:54:03 -0700

    added queue comments.

---
+++ JavaScriptAlgorithms/Queue.js
@@ -0,0 +1,17 @@
+// Implementing Queue data structure in javascript.
+// A Queue works on the FIFO(First in First Out) principle. Hence, it performs two basic operations that is addition of elements at the end of the queue and removal of elements from the front of the queue. Like Stack, Queue is also a linear data structure.
+
+// What we will be Implementing:
+// Functions to be implemented
+   // enqueue(item)
+   // dequeue()
+   // front()
+   // isEmpty()
+   // printQueue()
+
+class Queue {
+  constructor() {
+    this.items = [];
+
+  }
+}
