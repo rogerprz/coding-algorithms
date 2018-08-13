3fa77bf46bf3973503b6b060455b3b1337ab074f
rogercodes1 <roger.ucsb@gmail.com>
2018-08-13 16:16:33 -0700

    added class and constructor data.

--- JavaScriptAlgorithms/Graph-DataStructure.js
+++ JavaScriptAlgorithms/Graph-DataStructure.js
@@ -1,1 +1,18 @@
 //A Graph consists of a finite set of vertices(or nodes) and set of Edges which connect a pair of nodes.
+
+// functions to be implemented
+
+   // addVertex(v)
+   // addEdge(v, w)
+   // printGraph()
+
+   // bfs(v)
+   // dfs(v)
+   
+class Graph {
+  constructor(numOfVertices) {
+    this.numOfVertices = numOfVertices;
+    this.AdjList = new Map();
+
+  }
+}
