09ba402d7b960744d52c0b8245ebc575284219c0
rogercodes1 <roger.ucsb@gmail.com>
2018-08-13 16:24:40 -0700

    added addEdge function and notes.

--- JavaScriptAlgorithms/Graph-DataStructure.js
+++ JavaScriptAlgorithms/Graph-DataStructure.js
@@ -8,11 +8,28 @@
 
    // bfs(v)
    // dfs(v)
-   
+//Below we store the number of vertices in the graph and
+// AdjList, which stores a adjacency list of a particular vertex.
+// We used a Map Object provided by ES6 in order to implement Adjacency list.
+// Where key of a map holds a vertex and values holds an array of adjacent node
+
 class Graph {
   constructor(numOfVertices) {
     this.numOfVertices = numOfVertices;
     this.AdjList = new Map();
 
+  }
+  addVertex(v
+    // initialize the adjacent list with a
+    // null array
+    this.AdList.set(v,[]);
+  }
+  addEdge(v, w){
+    // get the list for vertex v and put the
+    // vertex w denoting edge betweeen v and w
+    this.AdjList.get(v).push(w);
+    // Since graph is undirected,
+   // add an edge from w to w also
+    this.AdjList.get(w).push(v);
   }
 }
