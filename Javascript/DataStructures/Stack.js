ab082c26f27933648ebc12a5b7986f0eafde4628
rogercodes1 <roger.ucsb@gmail.com>
2018-08-13 20:28:16 -0700

    added testing material for stack.

--- Javascript/DataStructures/Stack.js
+++ Javascript/DataStructures/Stack.js
@@ -20,5 +20,49 @@ class Stack {
     //returns top most element to view not delete
     return this.items[this.items.length-1]
   }
+  // Helper methods
+  isEmpty(){
+    //true if stack is empty
+    return this.items.length === 0;
+  }
+  printStack(){
+    let str = '';
+    for (let i = 0; i < this.items.length; i++) {
+      str += this.items[i] + ' ';
+
+    }
+    return str
+  }
 
 }
+//TESTING THE STACK CLASS
+// creating object for stack class
+var stack = new Stack();
+
+// testing isEmpty and pop on an empty stack
+
+// returns false
+console.log(stack.isEmpty());
+
+// returns Underflow
+console.log(stack.pop());
+Some more functions of stack class
+Example :
+
+// Adding element to the stack
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+// Printing the stack element
+// prints [10, 20, 30]
+console.log(stack.printStack());
+
+// returns 30
+console.log(stack.peek());
+
+// returns 30 and remove it from stack
+console.log(stack.pop());
+
+// returns [10, 20]
+console.log(stack.printStack()); 
