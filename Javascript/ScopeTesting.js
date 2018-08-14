996083b975a3fbab8ed66342c6fb583a12358cc0
rogercodes1 <roger.ucsb@gmail.com>
2018-08-14 14:11:07 -0700

    tested scope using regular function and es6

--- Javascript/ScopeTesting.js
+++ Javascript/ScopeTesting.js
@@ -1,22 +1,25 @@
 const myFood = {
-      egg: “plant”,
+      egg: 'plant',
       func: function() {
-            var self = this;
-            console.log(“outer func: this.egg = “ + this.egg);
-            console.log(“outer func: self.egg = “ + self.egg);
+            let self = this;
+            console.log("outer func: this.egg = " + this.egg);
+            console.log("outer func: self.egg = " + self.egg);
            (function() {
-            console.log(“inner func: this.egg = “ + this.egg);
-            console.log(“inner func: self.egg = “ + self.egg);
+            console.log("inner func: this.egg = " + this.egg);
+            console.log("inner func: self.egg = " + self.egg);
             }());
        },
-       es6Func: () =>{
-             let self = this;
+       es6Func: function(){
+             let es6Self = this;
              console.log("es6 outer func: this.egg = " + this.egg);
-             console.log("es6 outer func: self.egg = " + self.egg);
-            (() => {
+             console.log("es6 outer func: es6Self.egg = " + es6Self.egg);
+             debugger
+             (() => {
              console.log("es6 inner func: this.egg = " + this.egg);
-             console.log("es6 inner func: self.egg = " + self.egg);
-             }());
+             console.log("es6 inner func: es6Self.egg = " + es6Self.egg);
+           })();
         }
 };
 myFood.func();
+console.log('*'.repeat(35));
+myFood.es6Func();
