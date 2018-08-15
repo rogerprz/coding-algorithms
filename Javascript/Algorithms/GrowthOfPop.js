a559b8df8e4274f33a5b7131f0d2d9e3a94e231b
rogercodes1 <roger.ucsb@gmail.com>
2018-08-14 21:35:21 -0700

    completed nbYear problem.

--- Javascript/Algorithms/GrowthOfPop.js
+++ Javascript/Algorithms/GrowthOfPop.js
@@ -1,21 +1,22 @@
-// p0 = population
-//  percent increasing
-//aut = additional people
-
-let n = 0
-function nbYear(p0, percent, aug, p) {
-  console.log(n, p0);
+function nbYear(p0, percent, aug, p, n = 0){
     if (p0 >= p){
       return n
-      }
-    else {
+    }
+    else{
       n++
-      p0 = p0+(p0*(percent/100)) + aug
-      // debugger
-      return nbYear(p0, percent, aug, p)
-      }
+      p0+=(p0*(percent/100) + aug)
+      return nbYear(p0, percent, aug, p, n)
+    }
 }
+
+
+
+
 
 nbYear(1500, 5, 100, 5000) //15
 nbYear(1500000, 2.5, 10000, 2000000) //10
-nbYear(1500000, 0.25, 1000, 2000000) //94
+nbYear(1500000, 0.25, 1000, 2000000, 0) //94
+
+// function nbYear(p0, percent, aug, p, n = 0){
+//     (p0 >= p) ? n : ((n++, p0+=(p0*(percent/100)) + aug, nbYear(p0, percent, aug, p, n))
+//   }
