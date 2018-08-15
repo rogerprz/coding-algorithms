385e3d81c1da8139d3d9778ca816ffe26fa9265c
rogercodes1 <roger.ucsb@gmail.com>
2018-08-15 12:29:45 -0700

    completed binaryST class. working on insert for node class.

--- Javascript/Searches/BinaryTreeSearch.js
+++ Javascript/Searches/BinaryTreeSearch.js
@@ -2,10 +2,32 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value){
+   this.root === null ? this.root = Node.new(value) : this.root.insert(value)
+  }
+  treeSearch(value, node = this.root){
+    if (node === null){
+      return null
+    }
+    if (value < node.key){
+      return treeSearch(value, node.left)
+    } else if (value > node.key) {
+      return treeSearch( value, node.righ)
+    } else {
+      return node
+    }
+  }
 }
 
 class Node {
-  constructor(arguments) {
+  constructor(key = null) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
 
+  insert(newValue){
+    
   }
 }
