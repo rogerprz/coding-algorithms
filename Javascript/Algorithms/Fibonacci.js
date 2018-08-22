5a3a779f2e860851cc6fe80c50431775eaeed68e
rogercodes1 <roger.ucsb@gmail.com>
2018-08-21 20:38:28 -0700

    fibonacci solved.

--- Javascript/Algorithms/Fibonacci.js
+++ Javascript/Algorithms/Fibonacci.js
@@ -1,4 +1,21 @@
 function fibonacci(num) {
+  let arr = []
+  let a = 1, b = 0, temp
+  for (let i = 0; i< num; i++){
+    if (num === 0){
+      return num
+    } else if (num === 1){
+      return [0,1]
 
-
+    }
+    else {
+      temp = a
+      a = a + b
+      b = temp
+      arr.push(b)
+    }
+  }
+  return arr
 }
+
+fibonacci(5)
