f093b3fd836eea7bf286fa5e20fab6e2d3efde0f
rogercodes1 <roger.ucsb@gmail.com>
2018-08-26 12:25:06 -0700

    testing no change.

--- Javascript/CodeWars/ticketChange.js
+++ Javascript/CodeWars/ticketChange.js
@@ -2,22 +2,26 @@ function tickets(peopleInLine){
   let total = 0, noChange, change
   const cost = 25
   let count = 0
-  peopleInLine.forEach(money=>{
-    if (money === cost){
+  peopleInLine.forEach(payment=>{
+    mInHand = total
+    console.log(`${count+=1}`,'payment', payment);
+    console.log('mInHand',mInHand);
+    console.log('total', total);
+    debugger;
+    if (payment === cost){
+      noChange = 'YES'
       return total+=cost
     }
-    console.log(`${count+=1}`,'money', money);
-    change = money - (cost + total)
-    total+=cost
-    console.log('change',change);
-    console.log('total', total);
-    if (change < 0){
-      noChange = "NO"
-    } else {
+    else if (mInHand < payment){
+      return noChange = "NO"
+    }
+    else if((payment - mInHand) === 25){
+
       noChange = "YES"
     }
   })
   return noChange
 }
-console.log(tickets([25, 100])
-);
+
+// console.log(tickets([50,50,100]));
+console.log(tickets([25,25,25,25,25,100,100]));
