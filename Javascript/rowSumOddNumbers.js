fbeb40b6647abd7ccff4b21a5420ab33f4afbc99
rogercodes1 <roger.ucsb@gmail.com>
2018-08-25 22:18:10 -0700

    added alternate solution

--- Javascript/rowSumOddNumbers.js
+++ Javascript/rowSumOddNumbers.js
@@ -1,28 +1,23 @@
 function rowSumOddNumbers(n){
-  let items = 1
-  let odds = 1, temp
   if (n === 1){return n}
+  let items = 1, odds = 1, temp, arr = []
   let pos = sumAllOfN(n)
-  debugger;
-  for (let i = 1;i<=n;i++){
+  for (let i = 1;i<=(pos);i++){
+    if (i >(pos-n)){ arr.push(odds)}
     odds+=2
   }
-  // console.log("check odds: ",odds);
-  let total = odds
-  for (let j = 1; j<n; j++){
-    temp = total
-    odds+= 2
-    total = temp + odds
-  }
-  return total
+  return reduceArr(arr)
+}
+
+function reduceArr(arr) {
+  const reducer = (acc, currVal) => acc + currVal;
+  return arr.reduce(reducer,0)
 }
 
 function sumAllOfN(n) {
   let total = n
   n-=1
-  for (let i = 0; i < n; n--) {
-    total+=n
-  }
+  for (let i = 0; i < n; n--) {total+=n}
   return total
 }
 
@@ -34,3 +29,9 @@ rowSumOddNumbers(4)
 //3        7     9    11      === 4-6      27
 //4     13    15    17    19  === 7-10     64
 //5   21    23    25    27    29
+
+
+// solution using Math
+function rowSumOddNumbersPower(n) {
+  return Math.pow(n, 3);
+}
