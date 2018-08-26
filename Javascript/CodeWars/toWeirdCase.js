0152d186679fff375657a6ca88c32a9fe5a83433
rogercodes1 <roger.ucsb@gmail.com>
2018-08-25 23:27:38 -0700

    refactored solution.

--- Javascript/CodeWars/toWeirdCase.js
+++ Javascript/CodeWars/toWeirdCase.js
@@ -1,16 +1,12 @@
 function toWeirdCase(str){
   let arr = str.split(" ")
-  console.log(arr);
   let weirdArr = []
   for (let i = 0; i < arr.length; i++) {
-    // debugger;
     let word = arr[i].split("")
     for (let j = 0; j < word.length; j+=2) {
          word[j] = word[j].toUpperCase()
     }
-    console.log('arr word',word);
     weirdArr.push(word.join(""))
-
   }
   return weirdArr.join(" ")
 }
