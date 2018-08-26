3f37ddc87d0763d945ba5917c266513082bd97d6
rogercodes1 <roger.ucsb@gmail.com>
2018-08-25 23:19:05 -0700

    testing another option for toUpperCase.

---
+++ Javascript/CodeWars/toWeirdCase.js
@@ -0,0 +1,21 @@
+function toWeirdCase(str){
+  let arr = str.split(" ")
+  console.log(arr);
+  for (let i = 0; i < arr.length; i++) {
+    // debugger;
+
+  }
+  debugger
+}
+
+toWeirdCase('This is a test')
+
+// for (let j = 0; j < arr[i].length; j+=2) {
+//      arr[i].charAt(j).toUpperCase()
+//      arr[i]
+// }
+
+// arr[i].split("").map(x =>{
+//   x.toUpper
+//   debugger
+// })
