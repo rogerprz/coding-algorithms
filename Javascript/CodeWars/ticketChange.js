ad5aab8740d229760364584d51e5692f3fd79d71
rogercodes1 <roger.ucsb@gmail.com>
2018-08-26 12:32:13 -0700

    scrapping current solution.

--- Javascript/CodeWars/ticketChange.js
+++ Javascript/CodeWars/ticketChange.js
@@ -1,4 +1,12 @@
 function tickets(peopleInLine){
+
+}
+
+// console.log(tickets([50,50,100]));
+console.log(tickets([25,25,25,25,25,100,100]));
+
+
+function tickets(peopleInLine){
   let total = 0, noChange, change
   const cost = 25
   let count = 0
@@ -6,22 +14,20 @@ function tickets(peopleInLine){
     mInHand = total
     console.log(`${count+=1}`,'payment', payment);
     console.log('mInHand',mInHand);
-    console.log('total', total);
-    debugger;
+    // console.log('cost: ',cost,'total: ', total);
+
+    // debugger;
     if (payment === cost){
       noChange = 'YES'
       return total+=cost
     }
-    else if (mInHand < payment){
-      return noChange = "NO"
-    }
     else if((payment - mInHand) === 25){
-
       noChange = "YES"
     }
+    else if ((mInHand+cost) < payment){
+      return noChange = "NO"
+    }
+
   })
   return noChange
 }
-
-// console.log(tickets([50,50,100]));
-console.log(tickets([25,25,25,25,25,100,100]));
