288bd95128a461bd7a8a8ec4f56d74db53c1b0ed
rogercodes1 <roger.ucsb@gmail.com>
2018-08-25 21:48:19 -0700

    created function for sum all of n

--- Javascript/rowSumOddNumbers.js
+++ Javascript/rowSumOddNumbers.js
@@ -1,29 +1,36 @@
 function rowSumOddNumbers(n){
+  let items = 1
   let odds = 1, temp
+  if (n === 1){return n}
+  let pos = sumAllOfN(n)
+  debugger;
   for (let i = 1;i<=n;i++){
     odds+=2
   }
-  console.log("odds before 2nd", odds);
+  // console.log("check odds: ",odds);
   let total = odds
   for (let j = 1; j<n; j++){
-    // console.log("loop temp",temp);
     temp = total
-    // console.log("temp",temp);
     odds+= 2
-    // console.log("odds",odds);
     total = temp + odds
-    // console.log("total",total);
-    // debugger;
   }
-  console.log('what is total',total);
+  return total
+}
+
+function sumAllOfN(n) {
+  let total = n
+  n-=1
+  for (let i = 0; i < n; n--) {
+    total+=n
+  }
   return total
 }
 
 
-rowSumOddNumbers(3)
+rowSumOddNumbers(4)
 
-//1             1
-//2          3     5
-//3        7     9    11
-//4     13    15    17    19
+//1             1             === 1
+//2          3     5          === 2-3
+//3        7     9    11      === 4-6      27
+//4     13    15    17    19  === 7-10     64
 //5   21    23    25    27    29
