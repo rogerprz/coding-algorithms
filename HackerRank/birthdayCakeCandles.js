6c37d33aa49384c671317c518b26aa1354b65469
rogercodes1 <rogerperez.us@gmail.com>
2018-09-02 22:35:48 -0700

    finishe birthday cake candles problem.

--- HackerRank/birthdayCakeCandles.js
+++ HackerRank/birthdayCakeCandles.js
@@ -1,15 +1,11 @@
 let ar = [ 3, 2, 1, 3 ]
 let arr = [82, 49, 82, 82, 41, 82, 15, 63, 38, 25]
+let arr2 =[18, 90, 90, 13, 90, 75, 90, 8, 90, 43]
 function birthdayCakeCandles(ar) {
-  let count = 0, tallest, current
+  let count = 0, tallest = Math.max(...ar)
   ar.forEach(num => {
-
-    if (num >= tallest){
-      tallest = num
-      count+=1
-    }
+    if (num === tallest){count+=1}
   })
   return count
-
 }
-birthdayCakeCandles(arr)
+birthdayCakeCandles(arr2)
