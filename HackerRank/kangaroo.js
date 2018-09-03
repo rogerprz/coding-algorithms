b9b8f2c68494e59617e981ac4b8f85545098a41b
rogercodes1 <rogerperez.us@gmail.com>
2018-09-03 15:56:48 -0700

    not all test cases passing.

--- HackerRank/kangaroo.js
+++ HackerRank/kangaroo.js
@@ -1,11 +1,16 @@
 function kangaroo(x1, v1, x2, v2) {
   let multiply = x1 * x2
-  if (v1 < v2 || v1 >= multiply){
+  let checkMulti = multiply * multiply
+  if (v1 < v2 || v1 >= checkMulti){
     return "NO"
   }
   else if (x1 === x2) {
     return "YES"
   }
   else{
-    kangaroo(x1+v1, v1, x2+v2, v2)
+    return kangaroo(x1+v1, v1, x2+v2, v2)
   }
+}
+
+kangaroo(0, 2 ,5 ,3)
+console.log(kangaroo(0, 2 ,5 ,3));
