7e6fab68b2354a292f8b1ac29714953f851c329f
rogercodes1 <rogerperez.us@gmail.com>
2018-09-02 21:47:40 -0700

    altered solution.

--- HackerRank/birthdayCakeCandles.js
+++ HackerRank/birthdayCakeCandles.js
@@ -1,4 +1,15 @@
+let ar = [ 3, 2, 1, 3 ]
+let arr = [82, 49, 82, 82, 41, 82, 15, 63, 38, 25]
 function birthdayCakeCandles(ar) {
+  let count = 0, tallest, current
+  ar.forEach(num => {
 
+    if (num >= tallest){
+      tallest = num
+      count+=1
+    }
+  })
+  return count
 
 }
+birthdayCakeCandles(arr)
