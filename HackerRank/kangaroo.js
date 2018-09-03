28a339ba884bc0067202dfabf26cc7e1a4258c2c
rogercodes1 <rogerperez.us@gmail.com>
2018-09-03 15:57:49 -0700

    updated order of if else statements.

--- HackerRank/kangaroo.js
+++ HackerRank/kangaroo.js
@@ -1,11 +1,11 @@
 function kangaroo(x1, v1, x2, v2) {
   let multiply = x1 * x2
   let checkMulti = multiply * multiply
-  if (v1 < v2 || v1 >= checkMulti){
-    return "NO"
-  }
-  else if (x1 === x2) {
+  if (x1 === x2){
     return "YES"
+  }
+  else if (v1 < v2 || v1 >= checkMulti){
+    return "NO"
   }
   else{
     return kangaroo(x1+v1, v1, x2+v2, v2)
