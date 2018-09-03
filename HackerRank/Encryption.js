3831ef92375e97204c486d94860ea9908cd08817
rogercodes1 <rogerperez.us@gmail.com>
2018-09-02 20:26:31 -0700

    added arrays encryption files.

--- HackerRank/Encryption.js
+++ HackerRank/Encryption.js
@@ -1,8 +1,14 @@
 // For example, the sentence , after removing spaces is  characters long.  is between  and , so it is written in the form of a grid with 7 rows and 8 columns.
-
+// console.log('hi');
 function encryption(s) {
   let arr = s.split("")
+  let arrays = []
   let start = Math.floor(Math.sqrt(arr.length))
+  while (arr.length > 0){
+    arrays.push(arr.splice(0, start));
+    console.log(arrays);
+  }
+
 
 }
 
