d87d0e1d7775e336abcd86dfe0389a3c1a5d773f
rogercodes1 <rogerperez.us@gmail.com>
2018-09-03 16:04:09 -0700

    all but 1 testcase 10. ugh.

--- HackerRank/kangaroo.js
+++ HackerRank/kangaroo.js
@@ -1,10 +1,11 @@
 function kangaroo(x1, v1, x2, v2) {
-  let multiply = x1 * x2
-  let checkMulti = multiply * multiply
+  console.log('x1: ',x1, ' x2: ', x2);
+  let multiply = v1 * v2
+  let checkMulti = multiply * 4
   if (x1 === x2){
     return "YES"
   }
-  else if (v1 < v2 || v1 >= checkMulti){
+  else if (v1 < v2 || x1 > x2){
     return "NO"
   }
   else{
@@ -13,4 +14,4 @@ function kangaroo(x1, v1, x2, v2) {
 }
 
 kangaroo(0, 2 ,5 ,3)
-console.log(kangaroo(0, 2 ,5 ,3));
+console.log(kangaroo(0, 3 ,4 ,2));
