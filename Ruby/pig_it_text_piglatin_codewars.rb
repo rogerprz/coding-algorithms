575bd636665bdaa071420a69318355da9244fc3f
rogercodes1 <rogerperez.us@gmail.com>
2018-10-23 09:46:58 -0700

    finished pit it text piglatin

---
+++ Ruby/pig_it_text_piglatin_codewars.rb
@@ -0,0 +1,19 @@
+def pig_it text
+  newArr = []
+  text.split(" ").each do |word|
+    newWord = word[1...word.size] + word[0] + "ay"
+    if word.size > 1
+      newArr << newWord
+    elsif word =~ /[a-zA-Z]/
+      newArr << newWord
+    else
+      newArr << word
+    end
+  end
+  newArr.join(" ")
+end
+
+
+def alt_pig_it text
+  text.gsub(/(\w)(\w+)*/, '\2\1ay')
+end
