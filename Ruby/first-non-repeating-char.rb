f575753261ad6e10ac35a19febca29393e5034f3
rogercodes1 <rogerperez.us@gmail.com>
2018-10-24 09:34:03 -0700

    added my own solution.

--- Ruby/first-non-repeating-char.rb
+++ Ruby/first-non-repeating-char.rb
@@ -1,4 +1,16 @@
-
+# my solution
+def  first_non_repeating_letter(s)
+  ogArr = s.split("")
+  arr = ogArr.map(&:downcase)
+  first = ""
+  arr.each_with_index do |elem, index|
+    if arr.count(elem) === 1
+      first = index
+      break
+    end
+  end
+   first === "" ? first : ogArr[first]
+end
 
 # def  first_non_repeating_letter(s)
 #   s.chars.find {|i| s.downcase.count(i)==1 || s.upcase.count(i)==1} || ""
