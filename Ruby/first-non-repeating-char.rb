d35ef364d3751e536ae5c3a6b5c0f4d508ad2f11
rogercodes1 <rogerperez.us@gmail.com>
2018-10-24 09:32:57 -0700

    added file and alt solutions.

---
+++ Ruby/first-non-repeating-char.rb
@@ -0,0 +1,9 @@
+
+
+# def  first_non_repeating_letter(s)
+#   s.chars.find {|i| s.downcase.count(i)==1 || s.upcase.count(i)==1} || ""
+# end
+# alt
+# def  first_non_repeating_letter(s)
+#   s.chars.find { |c| s.downcase.count(c.downcase) == 1 } || ""
+# end
