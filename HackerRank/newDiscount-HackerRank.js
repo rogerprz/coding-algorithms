b69119f577560f4ef7ad38432844d4a70cdd3dea
rogercodes1 <rogerperez.us@gmail.com>
2018-10-26 13:10:06 -0700

    figured out multiplying order values.

--- HackerRank/newDiscount-HackerRank.js
+++ HackerRank/newDiscount-HackerRank.js
@@ -7,24 +7,22 @@ const reducer = (acc, curr) => acc + curr;
 const multiplier = (acc, curr) => acc*curr
   // arr.reduce((acc, curr) => ((curr > 0) ? (acc*curr) : acc), 0)
     // return isNaN(acc) ? (acc * curr) : curr
-
-
 function quantityDiscount(items) {
-  debugger
     let itemPriceArr = []; let reducedArr = []
-//     variables used to determine new pricing model quantity and discount adjustment
     const totalQuantity = totalOrderQuantity(items)
     let reduced_quant_adjust = (totalQuantity) **-0.1
 
     items.forEach(item=>{
-//         variables saved to make code easier to read. Not necessarily needed
+      let itemValues = Object.values(item)
+      let constValues = itemValues.reduce((acc, curr) => ((!isNaN(curr)) ? (acc*curr) : acc), 1)
+      debugger
         let quant_adjust = item.quantity ** -0.1;
         let area = item.width * item.height;
         let quantity = item.quantity;
         let product_area_price = item.product.area_price;
 
 
-        let constValues = area * product_area_price * quantity
+        // let constValues = area * product_area_price * quantity
 
         let item_price =  constValues * quant_adjust
         let new_reduced_item_price = constValues * reduced_quant_adjust
@@ -55,8 +53,11 @@ const B2 = {"quantity": 100,"width": 1,"height": 1, "product": { "area_price": 0
 const B3 = {"quantity": 150,"width": 3,"height": 3, "product": { "area_price": 0.8 } }
 
 
-const C1 =[{"quantity": 50,"width": 2,"height": 2, "product": { "area_price": 0.6 } },{"quantity": 150,"width": 3,"height": 2, "product": { "area_price": 0.8 } },
-{"quantity": 50,"width": 2,"height": 2, "product": { "area_price": 0.8 } }]
+const C1 =[
+  {"quantity": 50,"width": 2,"height": 2, "product": { "area_price": 0.6 }},
+  {"quantity": 150,"width": 3,"height": 2, "product": { "area_price": 0.8 } },
+  {"quantity": 50,"width": 2,"height": 2, "product": { "area_price": 0.8 }
+}]
 
 
 quantityDiscount(C1)
