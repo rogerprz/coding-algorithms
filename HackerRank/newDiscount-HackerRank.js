a58bcd933f2ed19811e28ca91a3cf81faae08042
rogercodes1 <rogerperez.us@gmail.com>
2018-10-29 13:18:15 -0700

    file updated.

--- HackerRank/newDiscount-HackerRank.js
+++ HackerRank/newDiscount-HackerRank.js
@@ -8,7 +8,6 @@ function quantityDiscount(items) {
     const reduced_quant_adjust = (totalQuantity) **-0.1
 
     items.forEach(item=>{
-
       let itemArr = multiplyOrderValues(item)
       let constValuesTotal = itemArr.reduce(multiplier, 1) * item.product.area_price //multiplies quant,width, & height * area_price
       let quant_adjust = item.quantity ** -0.1;
@@ -18,7 +17,6 @@ function quantityDiscount(items) {
 
     let oldPriceTotal = (itemPriceArr.reduce(reducer, 0)).toFixed(2)
     let reducedTotal = (reducedPriceArr.reduce(reducer, 0)).toFixed(2)
-    let total = (oldPriceTotal-reducedTotal === 0) ? 0 : (oldPriceTotal-reducedTotal).toFixed(2)
     return (oldPriceTotal-reducedTotal === 0) ? 0 : (oldPriceTotal-reducedTotal).toFixed(2)
 }
 function totalOrderQuantity(items){
@@ -34,8 +32,10 @@ function multiplyOrderValues(item){
 }
 
 
-const A1 = { "quantity": 50,"width": 2,"height": 1, "product":{"area_price": 0.8 } }
-const A2 ={"quantity": 50,"width": 1, "height": 1, "product": { "area_price": 0.8 } }
+const A1 = [
+  {"quantity": 50,"width": 2,"height": 1, "product":{"area_price": 0.8 } },
+  {"quantity": 50,"width": 1, "height": 1, "product": { "area_price": 0.8 } }
+]
 
 const B1 = {"quantity": 50,"width": 2,"height": 1, "product": { "area_price": 0.8 } }
 const B2 = {"quantity": 100,"width": 1,"height": 1, "product": { "area_price": 0.8 } }
