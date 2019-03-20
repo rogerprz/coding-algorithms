45a7779e02749fd4b6305891a9d57c9120e854c5
Raul Perez <rogerperez.us@gmail.com>
2019-03-20 14:44:45 -0700

    saved new code files.

--- LeetCode/Single-Elem-In-Sorted-Arr.js
+++ LeetCode/Single-Elem-In-Sorted-Arr.js
@@ -1,19 +1,35 @@
-console.log("HERE!!!");
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var singleNonDuplicate = function(nums) {
     if (nums.length === 1) {return nums[0]}
 
 
     let left = 0;
+    let middle = determineMiddle(left, nums.length)
     let right = nums.length -1;
-    let middle = determineMiddle(left, right)
-    console.log(nums)
-    while (left < right){
+    let count = 0
+    // console.log("NUMS: ",nums)
 
-        console.log("LEFT:", left)
-        console.log("RIGHT:", right)
-        console.log("MID: ", middle)
-        debugger
-        if (nums[middle] == nums[middle+1]){
+    while ((left < right) || count < 8){
+        console.log("NUMS: ",nums)
+        console.log("LEFT  : ", left, "ELEM:", nums[left])
+        console.log("MID  : ", middle, "ELEM:", nums[middle])
+        console.log("RIGHT:", right, "ELEM:", nums[right])
+        console.log(" ");
+        if (nums[middle] !== nums[middle+1] && nums[middle] !== nums[middle-1]){
+          return nums[middle]
+        }
+        else if (nums.length === 3 || (right - left) === 2 ) {
+          if (nums[middle] === nums[middle+1]){
+            return nums[left];
+          }
+          else {
+            return nums[right];
+          }
+        }
+        else if (nums[middle] === nums[middle+1]){
             right = middle
             middle = determineMiddle(left, right)
         }
@@ -24,13 +40,61 @@ var singleNonDuplicate = function(nums) {
         else if (nums[middle] != nums[middle+1]){
             return nums[middle];
         }
+        count+=1;
     }
 };
 
 function determineMiddle(left, right){
-    return (right % 2 === 0) ?  ((left+right)/2): (Math.floor((right+left)/2))
+    return (Math.floor((right+left)/2))
+    // return (right % 2 === 0) ?  ((left+right)/2): (Math.floor((right+left)/2))
 }
 
 let arr = [1,1,2];
+let arr2 = [1,1,2,2,4,4,5,5,9]; // len: 9
+console.log(singleNonDuplicate(arr))
+console.log(singleNonDuplicate(arr2));
 
-singleNonDuplicate(arr)
+
+
+
+//
+// var singleNonDuplicate = function(nums) {
+//     if (nums.length === 1) {return nums[0]}
+//     if (nums.length === 0 ) { return []}
+//     let left = 0;
+//     let right = nums.length -1;
+//     let middle;
+//     // console.log("NUMS: ",nums)
+//
+//     while (left < right){
+//         middle = Math.floor((left+ right)/2)
+//         console.log("NUMS: ",nums)
+//         console.log("LEFT  : ", left, "ELEM:", nums[left])
+//         console.log("MID  : ", middle, "ELEM:", nums[middle])
+//         console.log("RIGHT:", right, "ELEM:", nums[right])
+//         console.log(" ");
+//
+//        if ((middle - left) % 2 === 0 ){
+//            if (nums[middle] == nums[middle-1]){
+//                right = middle - 2
+//            }
+//            else {
+//                if (nums[middle] !== nums[middle+1]){
+//                    return nums[middle]
+//                }
+//            }
+//        }
+//         else {
+//             if (nums[middle] === nums[middle -1]){
+//                 left = middle + 1
+//
+//             }
+//             else {
+//                 right = middle - 1
+//             }
+//         }
+//         return nums[right];
+//     }
+// };
+//
+//
