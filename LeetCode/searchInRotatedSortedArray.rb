b5676504a69fe738875887575881bdef73efce39
rogercodes1 <rogerperez.us@gmail.com>
2019-03-25 16:30:43 -0700

    2nd almost solution

--- LeetCode/searchInRotatedSortedArray.rb
+++ LeetCode/searchInRotatedSortedArray.rb
@@ -11,7 +11,7 @@ def search1(nums, target)
         else 
             return mid = -1
         end 
-        
+
         return mid if nums[mid-1]>nums[mid]
         if nums[mid-1] < nums[mid] && mid > nums[mid]
             right = mid-1
@@ -21,4 +21,22 @@ def search1(nums, target)
         
     end
     mid
+end
+
+def search2(nums, target)
+    l = 0
+    r = nums.size-1
+    m = ((l + r)/2).floor
+    
+    while l <= r do 
+        m = ((l + r)/2).floor
+        return -1 if l == r && nums[m] != target
+        return m if nums[m] == target
+        if nums[m] > target && m < nums[m]
+            l = m + 1
+        else 
+            r = m - 1
+        end 
+    end
+    m
 end
