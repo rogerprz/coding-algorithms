e88583be444dfcea2aa77302782d0acc7afc65f3
Roger Perez <rogerprz@github.com>
2022-02-20 18:02:20 -0800

    Time: 1063 ms (5.01%), Space: 59.8 MB (5.22%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let result =[]
+    for(let i = 0; i < nums.length-1; i++){
+        let currNum = nums[i]
+        
+        let up_nums = nums.slice(i+1)
+        console.log('Curr:', currNum, 'INDEX:', i, 'T:', target)
+        console.log('UN:', up_nums)
+        
+        const secNum = up_nums.indexOf(target - nums[i])
+        console.log('SecNum:', secNum)
+        
+        if (secNum > -1){
+            let changeIndex = secNum + i
+            return [i, changeIndex + 1]
+        }
+    }
+    // console.log('R:', result)
+    // return result
+};
