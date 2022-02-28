91d1ebe25ad35f8ae217dc6f39b2072b7ce7ff56
Roger Perez <rogerperez.us@gmail.com>
2022-02-27 21:51:03 -0800

    Update 228-summary-ranges.js

--- 228-summary-ranges/228-summary-ranges.js
+++ 228-summary-ranges/228-summary-ranges.js
@@ -28,3 +28,15 @@ var summaryRanges = function(nums) {
 
   return results
 };
+
+// Optimized solution
+// function summaryRanges(nums) {
+//   var res = [];
+//   for (var i = 0, left = nums[0]; i < nums.length; i++) {
+//     if (nums[i] + 1 !== nums[i + 1]) {
+//       res.push(left === nums[i] ? '' + nums[i] : left + '->' + nums[i]);
+//       left = nums[i + 1];
+//     }
+//   }
+//   return res;
+// }
