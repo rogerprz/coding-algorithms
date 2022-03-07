e0b87fffb95d8f07b0bbd12ba5d985d6694438e0
Roger Perez <rogerprz@github.com>
2022-03-06 22:57:26 -0800

    Time: 112 ms (19.51%), Space: 42 MB (53.59%) - LeetHub

---
+++ 100-same-tree/100-same-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+  let firstNode = p
+  let secNode = q
+  
+  console.log('START')
+  return dfs(p,q)
+  
+  function dfs(firstNode, secNode) {
+    if (!firstNode && !secNode) return true 
+    
+    if (!firstNode || !secNode || firstNode.val !== secNode.val) return false
+   
+    return dfs(firstNode.left, secNode.left) && dfs(firstNode.right, secNode.right)
+  }
+};
