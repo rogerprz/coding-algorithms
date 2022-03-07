de6fba308a7b8e4fddc98cbf0a50d89e0f8c666a
Roger Perez <rogerprz@github.com>
2022-03-07 05:22:16 +0000

    add function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log('HELLO')
+
+
+const large = new Array(100000).fill('nemo')
+
+function findNemo(array){
+  let t0= performance.now();
+  for (let i =0; i < array.lenght; i++) {
+    if(array[i] === 'nemo'){
+      console.log('Found NEMO!')
+    }
+  }
+
+  let t1 = performance.now()
+  console.log('Call to find nemo took' + (t1-t0) + " milliseconds")
+}
+
+findNemo(large)
