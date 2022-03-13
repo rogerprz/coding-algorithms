6f8af92b5624f9cabe3d1983de0b421648b4ec1d
Roger Perez <rogerprz@github.com>
2022-03-13 16:18:35 -0700

    Time: 124 ms (48.95%), Space: 52.1 MB (21.87%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -5,12 +5,11 @@
 var maxProfit = function(prices) {
   let maxProfit = 0;
   let min = prices[0];
+  
   for(let i = 1; i < prices.length; i++) {
     min = Math.min(prices[i], min);
     let currentProfit = prices[i] - min
-    if (maxProfit < currentProfit){
-      maxProfit = currentProfit
-    }
+    if (maxProfit < currentProfit) maxProfit = currentProfit
   }
   return maxProfit;
 };
