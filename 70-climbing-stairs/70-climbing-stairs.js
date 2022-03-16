cca5894e74a27c777c6b9203ee08ef6122031bf2
Roger Perez <rogerprz@github.com>
2022-03-15 22:11:30 -0700

    Time: 97 ms (27.10%), Space: 41.9 MB (51.79%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -11,3 +11,14 @@ var climbStairs = function(n) {
   }
   return arr[n];
 };
+
+// var climbStairs = function(n) {
+//     let a = 1, b = 2, next;
+    
+//     for(let i = 3; i <= n; i++) {
+//         next = a + b;
+//         a = b;
+//         b = next;
+//     }
+//     return n === 1 ? a : b;
+// };
