1ed5054e63dffbd6bcfdd78e4687a26048928760
Roger Perez <rogerprz@github.com>
2022-03-26 17:51:32 -0700

    Time: 58 ms (93.05%), Space: 42.1 MB (38.13%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -3,12 +3,10 @@
  * @return {number}
  */
 var average = function(salary) {
-    let sortedSalary = salary.sort((a,b) => a-b)
+  const sortedSalary = salary.sort((a,b) => a-b)
   sortedSalary.shift()
   sortedSalary.pop()
-  console.log('S:',sortedSalary)
-
-  let sum = sortedSalary.reduce((partialSum, a) => partialSum + a, 0)
-
-  return sum/sortedSalary.length
+  const sum = sortedSalary.reduce((partialSum, a) => partialSum + a, 0)
+  
+  return sum/salary.length
 };
