28308d4880481d37e92cfa0f176020c85e2eaaa0
Roger Perez <rogerprz@github.com>
2022-03-27 18:10:48 -0700

    Time: 72 ms (67.53%), Space: 43.5 MB (6.16%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -4,8 +4,14 @@
  */
 var subtractProductAndSum = function(n) {
   const arrayOfDigits = Array.from(String(n), Number)
-  let sum = arrayOfDigits.reduce((partialSum, a) => partialSum + a, 0)
-  let product = arrayOfDigits.reduce((partialSum, a) => partialSum * a, 1)
+  let sum = 0
+  let product = 1
+  for (const digit in arrayOfDigits){
+    sum = sum + arrayOfDigits[digit]
+    product = product * arrayOfDigits[digit]
+  }
+  // let sum = arrayOfDigits.reduce((partialSum, a) => partialSum + a, 0)
+  // let product = arrayOfDigits.reduce((partialSum, a) => partialSum * a, 1)
 
   console.log('SUM:', arrayOfDigits, sum, product)
   return product - sum
