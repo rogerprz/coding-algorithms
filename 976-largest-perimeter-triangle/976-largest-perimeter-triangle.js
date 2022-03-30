15bd0efaafd424359b6d50e45e6b6d0c46c04464
Roger Perez <rogerprz@github.com>
2022-03-29 23:06:42 -0700

    Time: 203 ms (6.55%), Space: 50.2 MB (5.18%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.js
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.js
@@ -4,11 +4,13 @@
  */
 var largestPerimeter = function(nums) {
   nums.sort((a, b) => a - b);
+  
   let i = nums.length - 1;
-  console.log('START:')
+  
+  console.log('START:', nums)
   while(i >= 0){
-    console.log('i:',i, 'E:',nums[i], nums[i-1], nums[i-2])
     if (nums[i] < nums[i-1] + nums[i-2]){
+      console.log('It is less:',nums[i] < nums[i-1] + nums[i-2] )
         return nums[i] + nums[i-1] + nums[i-2];
     } else {
       i--;
