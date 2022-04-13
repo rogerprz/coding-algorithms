c1f8ea45dab5d248c5bdd3849efe923e4bfb85d5
Roger Perez <ropere@microsoft.com>
2022-04-13 15:04:47 -0700

    added notes

---
+++ Udemy/linkedList.js
@@ -0,0 +1,49 @@
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {}
+//   }
+// };
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(101);
+myLinkedList.append(15);
+myLinkedList.append(120);
+myLinkedList.prepend(1);
+console.log(myLinkedList);
---
+++ Udemy/notes.md
@@ -0,0 +1,21 @@
+## Array
+
+## Hash Tables
+
+- Scattered over memory
+
+## LinkedLIst
+
+### Why LinkedList?
+
+- It has a lose structure
+- Insert and delete operations are O(1)
+- Order is compared to hash tables
+
+Big O notation:
+
+- prepend O(1)
+- append O(1)
+- lookup O(n)
+- insert O(n)
+- delete O(n)
