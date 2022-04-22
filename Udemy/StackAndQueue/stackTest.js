040f2794512bbc357f9626bfd752a1102a4f6dfb
Roger Perez <ropere@microsoft.com>
2022-04-22 11:37:22 -0700

    Add base class

---
+++ Udemy/StackAndQueue/stackTest.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+}
