a409884226b52a05385aa0b9ffc5de6bf446e0cc
Roger Perez <ropere@microsoft.com>
2022-04-22 16:07:14 -0700

    Udpated stack test

--- Udemy/StackAndQueue/stackTest.js
+++ Udemy/StackAndQueue/stackTest.js
@@ -5,21 +5,40 @@ class Node {
   }
 }
 
+class StackArray {
+  constructor() {
+    this.array = [];
+  }
+
+  peek() {
+    const topElem = this.array[this.array.length - 1];
+    console.log('PeekArray:', topElem);
+    return topElem;
+  }
+
+  push(value) {
+    this.array.push(value);
+
+    console.log(this.array);
+    return this;
+  }
+
+  pop() {
+    const poppedElem = this.array.pop();
+
+    console.log('PE:', poppedElem);
+    return poppedElem;
+  }
+}
+
 class StackLinkedList {
   constructor() {
     this.top = null;
     this.bottom = null;
     this.length = 0;
   }
-}
-
-class StackArray {
-  constructor() {
-    this.top = null;
-    this.bottom = null;
-    this.length = 0;
-  }
   peek() {
+    console.log('PEEK:', this.top.value);
     return this.top;
   }
   push(value) {
@@ -51,3 +70,24 @@ class StackArray {
     return poppedTop;
   }
 }
+
+// const newStackList = new StackLinkedList();
+
+// newStackList.push(2);
+// newStackList.push(12);
+// newStackList.push(23);
+// newStackList.peek();
+// console.log(newStackList);
+// newStackList.pop();
+
+// console.log(newStackList);
+const newStackArray = new StackArray();
+
+newStackArray.push(2);
+newStackArray.push(12);
+newStackArray.push(23);
+newStackArray.peek();
+console.log(newStackArray);
+newStackArray.pop();
+
+console.log(newStackArray);
