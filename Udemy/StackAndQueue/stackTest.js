eb6424e96d111fa52024b8aef66821c21eb9cf7f
Roger Perez <ropere@microsoft.com>
2022-04-22 12:09:01 -0700

    WIP pop

--- Udemy/StackAndQueue/stackTest.js
+++ Udemy/StackAndQueue/stackTest.js
@@ -5,10 +5,43 @@ class Node {
   }
 }
 
-class Stack {
+class StackLinkedList {
   constructor() {
     this.top = null;
     this.bottom = null;
     this.length = 0;
+  }
+}
+
+class StackArray {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const previousTop = this.top;
+      this.top = newNode;
+      this.top.next = previousTop;
+    }
+
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    this.top = this.top.next;
+
+    return this;
   }
 }
