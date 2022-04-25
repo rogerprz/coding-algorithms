c8e3eef8f0d3fcbaa46f33733ca6334f3bf04be1
Roger Perez <rogerprz@github.com>
2022-04-24 20:28:48 -0700

    Time: 181 ms (68.13%), Space: 78.2 MB (40.36%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+  const array = []
+  
+  if (!head.next){
+    return true
+  }
+  
+  
+  while (head){
+    array.push(head.val)
+    
+    head = head.next
+  }
+  let left = 0
+  let right = array.length-1
+  if (array.length ===2){
+    if (array[0] === array [1]){
+      return true
+    } else {
+      return false
+    }
+  }
+  for (let i = 0; i<= array.length/2; i++ ){
+    if (array[left] !== array[right] ) {
+        return false
+    }
+    left++
+    right--
+  }
+  return true
+};
