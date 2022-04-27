8e98d1da59ce3436d9496e9f1fa846c3348fcadd
Roger Perez <ropere@microsoft.com>
2022-04-26 23:07:59 -0700

    Add initial base code

---
+++ Udemy/Tree/BinarySearchTree.js
@@ -0,0 +1,37 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    // insert
+  }
+
+  lookup(value) {
+    // lookup
+  }
+}
+
+const tree = new BinarySearchTree();
+//     9
+//   4    20
+// 1  6 15  170
+tree.insert(9);
+
+JSON.stringify(traverse(tree.root));
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+
+  return tree;
+}
