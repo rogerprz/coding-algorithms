a101838d68dc75a46daca42a14bbd31d74411eb1
Roger Perez <ropere@microsoft.com>
2022-04-27 18:41:10 -0700

    Implement insert for BinarySearchTree

--- Udemy/Tree/BinarySearchTree.js
+++ Udemy/Tree/BinarySearchTree.js
@@ -12,7 +12,29 @@ class BinarySearchTree {
   }
 
   insert(value) {
-    // insert
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (currentNode) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+      return this;
+    }
   }
 
   lookup(value) {
@@ -25,8 +47,16 @@ const tree = new BinarySearchTree();
 //   4    20
 // 1  6 15  170
 tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(1);
+tree.insert(170);
+tree.insert(15);
 
-JSON.stringify(traverse(tree.root));
+console.log('T:', tree);
+const treeStr = JSON.stringify(traverse(tree.root));
+console.log('JSON:', treeStr);
 
 function traverse(node) {
   const tree = { value: node.value };
