e17f36c58cafa07cb212e216e1298d7fe7fce9ec
Roger Perez <ropere@microsoft.com>
2022-05-02 13:43:42 -0700

    Factorial Iterative

--- Udemy/Algorithms/Factorial.js
+++ Udemy/Algorithms/Factorial.js
@@ -1,7 +1,17 @@
 function factorialIterative(value) {
-  return value;
+  let result = value;
+  while (value > 1) {
+    value = value - 1;
+    console.log('RR:', result, value);
+
+    result = result * value;
+  }
+  console.log('R:', result);
+  return result;
 }
 
 function factorialRecursive(value) {
   return value;
 }
+
+factorialIterative(5);
