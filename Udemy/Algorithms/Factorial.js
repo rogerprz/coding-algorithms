12786c081b7afa19fd41150f76a71271a667f00e
Roger Perez <ropere@microsoft.com>
2022-05-03 10:19:20 -0700

    Add Factorial

--- Udemy/Algorithms/Factorial.js
+++ Udemy/Algorithms/Factorial.js
@@ -1,5 +1,9 @@
 function factorialIterative(value) {
   let result = value;
+
+  if (value <= 2) {
+    return value;
+  }
   while (value > 1) {
     value = value - 1;
     console.log('RR:', result, value);
@@ -10,8 +14,13 @@ function factorialIterative(value) {
   return result;
 }
 
-function factorialRecursive(value) {
-  return value;
+function factorialRecursive(number) {
+  if (number === 2) {
+    return 2;
+  }
+  return number * factorialRecursive(number - 1);
 }
 
 factorialIterative(5);
+
+console.log('Recursive: ', factorialRecursive(5));
