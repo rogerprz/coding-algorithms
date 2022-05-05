e14caf3d7515d4a81564cd8119e7d5fbf5d61af1
Roger Perez <ropere@microsoft.com>
2022-05-04 23:32:34 -0700

    Graph completion

--- Udemy/Graphs/graph.js
+++ Udemy/Graphs/graph.js
@@ -1,22 +1,3 @@
-// Edge list
-const graph = [
-  [0, 2],
-  [2, 3],
-  [2, 1],
-  [1, 3]
-];
-
-// Adjacent list
-const graphAdj = [[2], [2, 3], [0, 1, 2], [1, 2]];
-
-// Adjacent Matrix
-const graphMatrix = [
-  [0, 0, 1, 0],
-  [0, 0, 1, 1],
-  [1, 1, 0, 1],
-  [0, 1, 1, 0]
-];
-
 class Graph {
   constructor() {
     this.numberOfNodes = 0;
@@ -24,16 +5,25 @@ class Graph {
   }
 
   addVertex(node) {
-    return node;
+    // if (this.adjacentList[node]) {
+    //   this.adjacentList[node].push(node);
+    // } else {
+
+    // }
+    this.adjacentList[node] = [];
+    this.numberOfNodes++;
+
+    return this;
   }
 
   addEdge(node1, node2) {
-    return node1;
+    this.adjacentList[node1].push(node2);
+    this.adjacentList[node2].push(node1);
+    return this;
   }
 
   showConnections() {
-    const allNodes = Object.keys;
-    this.adjacentList;
+    const allNodes = Object.keys(this.adjacentList);
 
     for (let node of allNodes) {
       let nodeConnections = this.adjacentList[node];
@@ -55,13 +45,13 @@ myGraph.addVertex('2');
 myGraph.addVertex('3');
 myGraph.addVertex('4');
 myGraph.addVertex('5');
-
+myGraph.addVertex('6');
 myGraph.addEdge('3', '1');
 myGraph.addEdge('3', '4');
 myGraph.addEdge('4', '2');
 myGraph.addEdge('4', '5');
 myGraph.addEdge('1', '2');
-// myGraph.addEdge('4', '5');
-// myGraph.addEdge('4', '5');
-// myGraph.addEdge('4', '5');
+myGraph.addEdge('1', '0');
+myGraph.addEdge('0', '2');
+myGraph.addEdge('6', '5');
 myGraph.showConnections();
