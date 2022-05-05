3de08bb519629eaf61e3a63015c7fd68daf95cb9
Roger Perez <ropere@microsoft.com>
2022-05-04 23:39:56 -0700

    bubble sort completed

--- Udemy/Algorithms/reverseStringRecursion.js
+++ Udemy/Algorithms/reverseStringRecursion.js
@@ -1,3 +1,11 @@
-function reverseStrRecursion(str) {
-  return str;
+function reverseStrRecursion(str, count) {
+  console.log('SSS:', str, count);
+  if (str.length === count) {
+    return str;
+  }
+  const value = str.slice(0);
+  console.log('VVV:', value);
+  return reverseStrRecursion(str + value, count++);
 }
+
+console.log(reverseStrRecursion('Hello'));
