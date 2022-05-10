93c9fbbaf9ccf33cfa2c2e69b2347a2b858dfbb0
Roger Perez <rogerprz@github.com>
2022-05-09 22:37:17 -0700

    Time: 103 ms (54.60%), Space: 45.6 MB (44.93%) - LeetHub

---
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.js
@@ -0,0 +1,27 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function(root) {
+  return traverseTree(root, [])
+  
+  function traverseTree(node, list) {
+    if (!node) return list
+    
+    list.push(node.val)
+
+    for (child in node.children) {
+      traverseTree(node.children[child], list)
+    }
+    
+    return list
+  }
+};
