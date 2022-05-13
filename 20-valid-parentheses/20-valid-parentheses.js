60c345406a92027d4b7aa11647d271163bfcf6e3
Roger Perez <rogerprz@github.com>
2022-05-12 22:47:09 -0700

    Time: 59 ms (94.91%), Space: 42.3 MB (60.69%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -7,7 +7,11 @@ var isValid = function(s) {
   if (arr.length <= 1) return false
   
   let stack = [];
-  let obj = { "{": "}", "[": "]", "(": ")"}
+  let obj = {
+    "{": "}", 
+    "[": "]", 
+    "(": ")"
+  }
   let keys = Object.keys(obj)
   let values = Object.values(obj)
   
