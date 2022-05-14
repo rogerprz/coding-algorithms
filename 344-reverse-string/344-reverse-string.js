5c1d5807c2b817f8e75449912a26796881f25274
Roger Perez <rogerprz@github.com>
2022-05-14 13:36:32 -0700

    Time: 143 ms (28.99%), Space: 49.2 MB (78.80%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -5,14 +5,11 @@
 var reverseString = function(s) {
   let left = 0 
   let right = s.length - 1
-  console.log('LEN:', s.length)
   
   while (left < right){
-    console.log('L:', left, 'R:', right)
     let temp = s[left]
     s[left] = s[right]
     s[right] = temp
-    console.log('AS:', s)
 
     left++
     right--
