75e8136b4cd077355d6502699fc3b8d7ac97a38d
Roger Perez <rogerprz@github.com>
2022-05-14 18:50:10 -0700

    Time: 85 ms (57.73%), Space: 44.3 MB (53.73%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -14,9 +14,16 @@ var mergeTwoLists = function(l1, l2) {
   let list = new ListNode()
   let head = list
   
-  while (l1 && l2){
+  while (l1 || l2){
     
-    if (l1.val < l2.val){
+    if (!l1 && l2 ){
+      list.next = l2 
+      l2 = l2.next
+    } else if (l1 && !l2) {
+      list.next = l1
+      l1 = l1.next
+    }
+    else if (l1.val < l2.val){
       list.next = new ListNode(l1.val)
       l1 = l1.next
     } else {
@@ -26,8 +33,8 @@ var mergeTwoLists = function(l1, l2) {
     list = list.next
   }
   
-  if (l1 !== null) list.next = l1
-  if (l2 !== null ) list.next = l2
+  // if (l1 !== null) list.next = l1
+  // if (l2 !== null ) list.next = l2
   
   return head.next
 };
