43eec4e1e5222819f7e6899ed936710bcc5f958d
Roger Perez <rogerprz@github.com>
2022-05-15 14:16:21 -0700

    Time: 127 ms (47.33%), Space: 50.5 MB (75.88%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,13 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
+  
   return nums.length !== new Set(nums).size
 };
-
-//  for (let i = 0; i < nums.length; i ++){
-//       let currElem = nums[i]
-//       let testArr = nums.slice(i+1)
-      
-//       if (testArr.indexOf(currElem) > -1) return true
-//     }
-//   return false
