f90aecba85c57c36897fe6f7086022238bb58356
Roger Perez <rogerprz@github.com>
2022-05-15 20:55:34 -0700

    Time: 171 ms (52.82%), Space: 47.6 MB (57.50%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -4,13 +4,13 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
- const dp = Array(amount + 1).fill(Infinity);
+  const dp = Array(amount + 1).fill(Infinity);
   dp[0] = 0;
   for (let i = 1; i <= amount; i++) {
     for (const coin of coins) {
       if (i - coin >= 0) {
         dp[i] = Math.min(
-          dp[i],
+          dp[i], 
           dp[i - coin] + 1,
         );
       }
