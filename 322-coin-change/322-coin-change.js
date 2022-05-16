114285c3ae942e6cfbaaf09603bcb452993db712
Roger Perez <rogerprz@github.com>
2022-05-15 21:16:58 -0700

    Time: 116 ms (87.26%), Space: 48 MB (49.29%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -3,20 +3,15 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function(coins, amount) {
-  const dp = Array(amount + 1).fill(Infinity);
-  dp[0] = 0;
-  for (let i = 1; i <= amount; i++) {
-    for (const coin of coins) {
-      if (i - coin >= 0) {
-        dp[i] = Math.min(
-          dp[i], 
-          dp[i - coin] + 1,
-        );
-      }
+const coinChange = (coins, amount) => {
+  const dp = Array(amount + 1).fill(Infinity); // This arr tells us how many coins we need for each amount.
+  dp[0] = 0; // To make 0, we need 0 coins.
+  for (let coin of coins) { // Check each coin
+    for (let i = coin; i <= amount; i++) { // Iterate through the entire amount from coin
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1); // Update minimum number of needed coins.
     }
   }
-  return dp[amount] === Infinity ? -1 : dp[amount];
+  return dp[amount] === Infinity ? -1 : dp[amount]; // If the last element is Infinity, then we cannot make the amount.
 };
 
 // NOT MY SOLUTION
