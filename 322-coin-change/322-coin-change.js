aacff2eb1a8444c8515b51627e9a68fbe43b6581
Roger Perez <rogerprz@github.com>
2022-05-15 20:54:31 -0700

    Time: 164 ms (56.20%), Space: 48.1 MB (47.14%) - LeetHub

---
+++ 322-coin-change/322-coin-change.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+ const dp = Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (let i = 1; i <= amount; i++) {
+    for (const coin of coins) {
+      if (i - coin >= 0) {
+        dp[i] = Math.min(
+          dp[i],
+          dp[i - coin] + 1,
+        );
+      }
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount];
+};
+
+// NOT MY SOLUTION
+// var coinChange = function(coins, amount) {
+//   // dp[i] represents the least amount of coins that can make the value equals to the i
+//   const dp = Array(amount + 1).fill(Infinity);
+//   dp[0] = 0;
+//   for (let i = 1; i <= amount; i++) {
+//     for (const coin of coins) {
+//       if (i - coin >= 0) {
+//         dp[i] = Math.min(
+//           dp[i],
+//           dp[i - coin] + 1,
+//         );
+//       }
+//     }
+//   }
+//   return dp[amount] === Infinity ? -1 : dp[amount];
+// };
