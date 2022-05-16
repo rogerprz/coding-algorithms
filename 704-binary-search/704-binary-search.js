12b3ea36245b8db39fcb5bacbd9664bad0631316
Roger Perez <rogerprz@github.com>
2022-05-15 19:41:36 -0700

    Time: 126 ms (5.72%), Space: 45.2 MB (49.02%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,24 +4,22 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let l = 0, r = nums.length - 1
-    let count = 0
-    while (l <= r ){
+    let l= 0, r = nums.length-1
+    
+    while (l<=r){
+//       get a middle point
 
       let m = Math.floor((l+r)/2)
-      let value = nums[m]
-      console.log('L:', l , 'R:', r, 'M:', m, 'C:', count)
-      console.log('V:',nums[m], '==',target )
-
-      if (target === nums[m]) return m
-      if (nums[m] < target){
-        l = m + 1
+      let num = nums[m]
+      
+//       mid === target return mid index
+      if (num === target) return m
+      
+      if (num < target){
+        l = m +1
       } else {
         r = m - 1
       }
     }
   return -1
 };
-//  0,1,2,3, 4,5
-// [-1,0,3,5,9,12]
-//   l 0 m 0 0 r
