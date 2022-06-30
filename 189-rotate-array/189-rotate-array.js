c3f69b4e068bc0a0d5a1004dbbd1783d4f7d808d
Roger Perez <rogerprz@github.com>
2022-06-29 21:39:57 -0700

    Time: 140 ms (47.24%), Space: 51.3 MB (74.61%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  if (k === 0) return nums;
+
+  k = k % nums.length;
+  nums.unshift(...nums.splice(nums.length - k));
+  return nums;
+};
