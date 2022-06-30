8107953ec1323c1f0e7c0e00e0f52edf53535c3c
Roger Perez <rogerprz@github.com>
2022-06-29 20:12:18 -0700

    Time: 126 ms (74.51%), Space: 48.3 MB (85.75%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -4,8 +4,8 @@
  */
 var sortedSquares = function (nums) {
   const results = nums.map((num) => {
-    const value = Math.abs(num);
-    return value ** 2;
+
+    return Math.abs(num) ** 2;
   });
 
   return results.sort((a, b) => a - b);
