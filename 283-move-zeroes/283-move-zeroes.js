4a14f09af9e3d59c837baa0a1b88a2a88804e417
Roger Perez <rogerprz@github.com>
2022-07-07 11:21:54 -0700

    Time: 122 ms (68.33%), Space: 46.5 MB (68.45%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -2,18 +2,21 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function (nums) {
-  if (nums.length <= 1) return nums;
-
-  let pos = 0;
-
+var moveZeroes = function(nums) {
+    if (nums.length <= 1) return nums
+  
+  let pos = 0 
+  
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      nums[pos++] = nums[i];
-    }
-  }
-  for (i = pos; i < nums.length; i++) {
-    nums[i] = 0;
+    if (nums[i] === 0) continue
+    // change nums[pos] to == curreElem
+    nums[pos] = nums[i]
+    // increase position. 
+    pos++
+   }
+  
+  for (let i = pos; i < nums.length; i ++){
+    nums[i] = 0
   }
   return nums
 };
