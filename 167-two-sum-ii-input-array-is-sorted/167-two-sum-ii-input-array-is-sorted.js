06821e4a28b2f239b8529f5d9fde15a13e4e0006
Roger Perez <rogerprz@github.com>
2022-07-10 14:53:24 -0700

    Time: 552 ms (5.01%), Space: 50.8 MB (5.66%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -4,18 +4,19 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let test = {};
-
-  for (let i = 0; i < nums.length; i++) {
-    let currentValue = nums[i];
-    let secondValue = target - currentValue;
-    if (secondValue in test) {
-      let result = [test[secondValue], i + 1];
-      return result;
-    } else {
-      test[currentValue] = i + 1;
+    let hash = {}
+    
+    for (let i = 0; i < nums.length; i++) {
+      let num = nums[i]
+      let secondNum = target-num
+      console.log('T:', target, num, secondNum)
+      if (secondNum in hash){
+        return  [hash[secondNum], ++i]
+          }
+      else {
+        hash[num] = i + 1
+      }
     }
-  }
 };
 
 // var twoSum = function(nums, target) {
