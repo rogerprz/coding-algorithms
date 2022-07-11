497c8eb65c273c76339b137e48e1fba816964ca3
Roger Perez <rogerprz@github.com>
2022-07-11 10:38:50 -0700

    Time: 590 ms (6.61%), Space: 64.2 MB (5.29%) - LeetHub

---
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+  let vowels = ['a','e','i','o','u', 'A','E','I','O','U']
+  let l = 0, r = s.length-1
+  
+  arr = s.split('')
+  while (l < r ) {
+    console.log('L:', l, 'R:', r)
+    if (vowels.includes(arr[l]) && vowels.includes(arr[r])) {
+      console.log('HERE')
+      // [arr[l], arr[r]] = [arr[r], arr[l]];
+      let temp = arr[l]
+      arr[l] = arr[r]
+      arr[r] = temp
+      l++, r--
+    }
+    if (!vowels.includes(arr[l])){
+      l++
+    }
+    else if (!vowels.includes(arr[r])){
+      r--
+    }
+  }
+  return arr.join('')
+};
