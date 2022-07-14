049bbe7ef52ac35bc1c543ffcf5c89029dd56fb9
Roger Perez <rogerprz@github.com>
2022-07-13 23:35:11 -0700

    Time: 167 ms (42.64%), Space: 49 MB (30.25%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -9,16 +9,18 @@ var lengthOfLongestSubstring = function(s) {
   let max_str = 0
     
     s.split("").forEach((str)=>{
-      if (result.includes(str)){
-          if (max_str < result.length) max_str = result.length 
-          result.push(str)
-
-          const strIndexOf = result.indexOf(str)
-
-          result = result.slice(strIndexOf + 1)
-      } else {
-          result.push(str)
-      }
+        if (result.includes(str)){
+            if (max_str < result.length){
+                max_str = result.length 
+            }
+            result.push(str)
+            
+            const strIndexOf = result.indexOf(str)
+            
+            result = result.slice(strIndexOf + 1)
+        } else {
+            result.push(str)
+        }
     })
    
     if (result.length > max_str) max_str = result.length
