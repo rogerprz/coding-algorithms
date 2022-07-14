845d4bc500c80250bd18f32d6b983321f01718d4
Roger Perez <rogerprz@github.com>
2022-07-13 23:28:00 -0700

    Time: 109 ms (82.62%), Space: 48.6 MB (39.50%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  result = []
+    max_str = 0
+    
+    s.split("").forEach((str)=>{
+        if (result.includes(str)){
+            if (max_str < result.length){
+                max_str = result.length 
+            }
+            result.push(str)
+            
+            const strIndexOf = result.indexOf(str)
+            
+            result = result.slice(strIndexOf + 1)
+        } else {
+            result.push(str)
+        }
+    })
+   
+    if (result.length > max_str) max_str = result.length
+    return max_str
+  
+};
