bef4831098acb5c1361a1a55052972cc151e8acc
Roger Perez <rogerprz@github.com>
2022-07-13 23:29:40 -0700

    Time: 142 ms (56.91%), Space: 48.7 MB (36.47%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -2,23 +2,23 @@
  * @param {string} s
  * @return {number}
  */
+
+// OLD SOLUTION 
 var lengthOfLongestSubstring = function(s) {
-  result = []
-    max_str = 0
+  let result = []
+  let max_str = 0
     
     s.split("").forEach((str)=>{
-        if (result.includes(str)){
-            if (max_str < result.length){
-                max_str = result.length 
-            }
-            result.push(str)
-            
-            const strIndexOf = result.indexOf(str)
-            
-            result = result.slice(strIndexOf + 1)
-        } else {
-            result.push(str)
-        }
+      if (result.includes(str)){
+          if (max_str < result.length) max_str = result.length 
+          result.push(str)
+
+          const strIndexOf = result.indexOf(str)
+
+          result = result.slice(strIndexOf + 1)
+      } else {
+          result.push(str)
+      }
     })
    
     if (result.length > max_str) max_str = result.length
