151dc4265b612bf5206bb88d5d7fb356f5d1e971
Roger Perez <rogerprz@github.com>
2022-07-15 15:15:30 -0700

    Time: 68 ms (94.44%), Space: 44.9 MB (30.96%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.js
+++ 941-valid-mountain-array/941-valid-mountain-array.js
@@ -6,13 +6,14 @@ var validMountainArray = function(arr) {
   if (arr.length < 3) return false
 
   let isGoingUp = true
-  let p1 = 0
-  let p2 = 1
+  let p1 = 0, p2 = 1
+  
   for (let i = 0; i < arr.length - 1; i++) {
     let start = arr[p1], next = arr[p2]
+    
     if (i > 0 && isGoingUp && start > next) isGoingUp = false
     if (start === next ) return false // 
-    if ((start < next && isGoingUp) || start > next && !isGoingUp) { // 
+    else if ((start < next && isGoingUp) || start > next && !isGoingUp) { // 
       p1++
       p2++
     }
