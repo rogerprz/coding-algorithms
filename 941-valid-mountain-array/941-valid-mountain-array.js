05c8370f67cfd8093dfcdb68ba9a90740732a360
Roger Perez <rogerprz@github.com>
2022-07-17 16:23:28 -0700

    Time: 76 ms (83.58%), Space: 44.9 MB (39.54%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.js
+++ 941-valid-mountain-array/941-valid-mountain-array.js
@@ -10,17 +10,13 @@ var validMountainArray = function(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     let start = arr[i], next = arr[i+1]
     
-//     Peek has been reached and we need to start going down
     if (i > 0 && isGoingUp && start > next) isGoingUp = false
-//  We can't have equal values
-    if (start === next ) return false // 
-//  If we are going up the mountain isGoingUp = true, when we start going down we switch it to false
-    else if ((start < next && isGoingUp) || start > next && !isGoingUp) { // 
+    if (start === next ) return false
+    else if ((start < next && isGoingUp) || start > next && !isGoingUp) { 
     }
     else {
       return false
     }
   }
-//   if we stayed going up and never went down then we return false, else true
   return isGoingUp ? false : true
 };
