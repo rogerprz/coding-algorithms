875f2484b1da192ea91bb2ecad051f1f775a5628
Roger Perez <rogerprz@github.com>
2022-07-19 14:51:32 -0700

    Time: 83 ms (54.51%), Space: 41.8 MB (82.83%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.js
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -3,13 +3,6 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-//          1  - level 0        1 Total: 1
-//        1  1 - level 1        2 Total: 2
-      // 1  2  1 - level 2      3 Total: 4
-    // 1  3   3  1 - level 3    4 Total: 8
-//   1  4   6 4   1 - level 4   5 Total: 16
-//   At each level we add 1 number
-//   aside from level 0 and 1 each level after that correspons with the 1 + previous level
   let result = []
   
   for (let i = 0; i < numRows; i++) {
@@ -17,8 +10,8 @@ var generate = function(numRows) {
     else if (i === 1) result.push([1, 1])
     
    else {
-      let currentArray = [1] // 1 3 3
-      let lastArr = result[result.length-1] // last array in result
+      let currentArray = [1]
+      let lastArr = result[result.length-1] 
 
       for (let j = 0; j < i-1; j++){ 
         let sum = lastArr[j]+lastArr[j+1]
