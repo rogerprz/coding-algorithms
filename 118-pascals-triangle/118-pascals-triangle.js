608f40d5f7490ecc35f8bbd256bd10c72574f8d8
Roger Perez <rogerprz@github.com>
2022-07-19 14:50:58 -0700

    Time: 72 ms (74.92%), Space: 41.6 MB (94.67%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.js
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -15,19 +15,18 @@ var generate = function(numRows) {
   for (let i = 0; i < numRows; i++) {
     if (i === 0) result.push([1])
     else if (i === 1) result.push([1, 1])
-    else {
+    
+   else {
       let currentArray = [1] // 1 3 3
       let lastArr = result[result.length-1] // last array in result
 
       for (let j = 0; j < i-1; j++){ 
         let sum = lastArr[j]+lastArr[j+1]
         currentArray.push(sum)
-        
       }
       currentArray.push(1)
       result.push(currentArray)
-    }
-
+     }
   }
   
   return result
