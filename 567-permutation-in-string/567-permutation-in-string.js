1caa946c1c543aaee10227f47c79fd3244f5f0aa
Roger Perez <rogerprz@github.com>
2022-07-18 23:03:28 -0700

    Time: 123 ms (67.04%), Space: 47.9 MB (46.09%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.js
@@ -0,0 +1,48 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+// NOT MY SOLUTION
+var checkInclusion = function (pattern, str) {
+  let windowStart = 0;
+  let matched = 0;
+  let charFreq = {};
+
+  for (let i = 0; i < pattern.length; i++) {
+    let char = pattern[i];
+    if (charFreq[char] === undefined) {
+      charFreq[char] = 1;
+    } else {
+      charFreq[char]++;
+    }
+  }
+
+  for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+    let rightChar = str[windowEnd];
+
+    if (rightChar in charFreq) {
+      charFreq[rightChar] -= 1;
+      if (charFreq[rightChar] === 0) {
+        matched += 1;
+      }
+    }
+
+    if (matched === Object.keys(charFreq).length) {
+      return true;
+    }
+
+    if (windowEnd >= pattern.length - 1) {
+      let leftChar = str[windowStart];
+      windowStart += 1;
+      if (leftChar in charFreq) {
+        if (charFreq[leftChar] === 0) {
+          matched -= 1;
+        }
+        charFreq[leftChar] += 1;
+      }
+    }
+  }
+
+  return false;
+};
