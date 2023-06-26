e89dcfbecba2bf1278e224adaeefad7f2bdd93ec
Roger Perez <rogerprz@github.com>
2023-06-25 20:30:22 -0700

    Time: 108 ms (56.24%), Space: 54.3 MB (60.11%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,43 +3,22 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    // we can create a new array
-    const leftArr = []
-    const rightArr = []
-    const results = []
+  const length = nums.length;
+  const result = new Array(length).fill(1);
 
-    let left = 1
-    let right = 1
-    const len = nums.length - 1
-    let rIndex = nums.length -2 // we dont need to mess with the last elem, lIndex will
-    let lIndex = -1;
+  // Calculate the product of elements before the current index
+  let productBefore = 1;
+  for (let i = 0; i < length; i++) {
+    result[i] *= productBefore;
+    productBefore *= nums[i];
+  }
 
-    for (let i = 0; i< nums.length; i++){
-        left = left * nums[i]
-        leftArr.push(left)
-    }
+  // Calculate the product of elements after the current index
+  let productAfter = 1;
+  for (let i = length - 1; i >= 0; i--) {
+    result[i] *= productAfter;
+    productAfter *= nums[i];
+  }
 
-    for (let i = len; i > -1;i--) {
-        right = right * nums[i]
-        rightArr.push(right)
-    }
-    console.log("S:", leftArr, rightArr)
-
-    for (let i = 0; i<nums.length; i++) {
-        const lElem = leftArr[lIndex];
-        const rElem = rightArr[rIndex];
-        // console.log('_____________________________')
-        // console.log('L:', lElem, leftArr)
-        // console.log('R:', rElem, rightArr)
-        const res = 
-            (lIndex == -1 ? 1 : lElem) *
-            (rIndex == -1 ? 1 : rElem);
-
-        results.push(res)
-        rIndex--;
-        lIndex++;
-    }
-
-    return results;
-    
+  return result;
 };
