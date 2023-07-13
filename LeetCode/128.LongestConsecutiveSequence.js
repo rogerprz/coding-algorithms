8c32fe5d8e4b0da8e609788a154afd75d3a94ebf
rogercodes1 <rogerperez.us@gmail.com>
2023-07-12 23:35:36 -0700

    128 MVP

--- LeetCode/128.LongestConsecutiveSequence.js
+++ LeetCode/128.LongestConsecutiveSequence.js
@@ -1,30 +1,21 @@
 const longestConsecutive = function (nums) {
   if (nums.length <= 1) return nums.length;
-  const sortedNums = nums.sort((a, b) => a - b);
+  nums.sort((a, b) => a - b);
+  let count = 0;
+  let temp = 1;
 
-  let previousPointer = sortedNums[0];
-  let leftIndex = 0;
-  let rightIndex = 0;
-  let maxLen = 1;
-
-  for (let i = 1; i < sortedNums.length; i++) {
-    const currentNum = sortedNums[i];
-    const testPointer = previousPointer + 1;
-    if (testPointer === currentNum) {
-      rightIndex = i;
-      previousPointer = currentNum;
-    } else if (previousPointer == currentNum) {
-      leftIndex = i + 1;
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] === nums[i + 1] - 1) {
+      temp++;
+      count = Math.max(count, temp);
+    } else if (nums[i] === nums[i + 1]) {
+      count = Math.max(count, temp);
     } else {
-      leftIndex = i;
-    }
-    const newMaxLen = rightIndex + 1 - leftIndex;
-    if (newMaxLen > maxLen) {
-      maxLen = newMaxLen;
+      temp = 1;
+      count = Math.max(count, temp);
     }
   }
-
-  return maxLen;
+  return count;
 };
 
 const nums = [100, 4, 200, 1, 3, 2];
