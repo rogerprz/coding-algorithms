f69cc3f878b1dcd5e441a4e0c2754b059d4428e6
Roger Perez <ropere@microsoft.com>
2023-07-16 14:58:02 -0700

    MVP

--- LeetCode/167.TwoSumII-InputArrayIsSorted.js
+++ LeetCode/167.TwoSumII-InputArrayIsSorted.js
@@ -1,4 +1,17 @@
-const twoSum = function (numbers, target) {};
+const twoSum = (numbers, target) => {
+  const map = new Map();
+
+  for (let i = 0; i < numbers.length; i++) {
+    const currNum = numbers[i];
+    const missingNum = target - currNum;
+    if (map.has(currNum)) {
+      return [map.get(currNum), i + 1];
+    }
+    map.set(missingNum, i + 1);
+  }
+
+  return map;
+};
 
 const numbers = [2, 7, 11, 15];
 const target = 9;
