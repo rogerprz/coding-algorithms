22ec4f0fb0f0edfb9c10275bbd4c7046ad38409b
Roger Perez <ropere@microsoft.com>
2023-07-21 15:35:50 -0700

    works but not right time complexity

--- LeetCode/11.ContainerWithMostWater.js
+++ LeetCode/11.ContainerWithMostWater.js
@@ -1,5 +1,24 @@
 const maxArea = function (height) {
-  return height;
+  // we will need a left and right pointer
+  // we will need to add the sum of water
+  // keep track of length j - i
+  // max height if j < k ? j : k
+  let mostWater = 0;
+
+  for (let i = 0; i < height.length; i++) {
+    const leftHeight = height[i];
+    for (let j = i + 1; j < height.length; j++) {
+      const rightHeight = height[j];
+
+      const length = j - i;
+      const bestHeight = leftHeight < rightHeight ? leftHeight : rightHeight;
+      const currentArea = bestHeight * length;
+
+      mostWater = Math.max(currentArea, mostWater);
+    }
+  }
+
+  return mostWater;
 };
 
 // Input:
