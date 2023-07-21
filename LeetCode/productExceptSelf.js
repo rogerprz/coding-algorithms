daa88847ef6f70429d1c4c94480f7bac71f3ec99
Roger Perez <ropere@microsoft.com>
2023-07-20 21:03:41 -0700

    fix productExceptSelft

--- LeetCode/productExceptSelf.js
+++ LeetCode/productExceptSelf.js
@@ -1,31 +1,25 @@
-var productExceptSelf = function (nums) {
-  const length = nums.length;
+const productExceptSelf = function (nums) {
+  const { length } = nums;
   const result = new Array(length).fill(1);
 
   // Calculate the product of elements before the current index
   let productBefore = 1;
   for (let i = 0; i < length; i++) {
-    console.log('R:', result, 'I:', i);
     const currentNumber = nums[i];
     const currentResult = result[i];
+
     result[i] = currentResult * productBefore; // result[i] *= productBefore;
-    console.log('AR:', result);
-    productBefore = productBefore * currentNumber; // productBefore *= nums[i];
-    console.log('APB:', productBefore);
+    productBefore *= currentNumber; // productBefore *= nums[i];
   }
   console.log('___________________________');
   // Calculate the product of elements after the current index
   let productAfter = 1;
   for (let i = length - 1; i >= 0; i--) {
-    console.log('R:', result, 'I:', i);
     const currentNumber = nums[i];
     const currentResult = result[i];
 
     result[i] = currentResult * productAfter;
-    console.log('AR:', result, result[i]);
-
-    productAfter = currentNumber * productAfter;
-    console.log('APB:', productAfter);
+    productAfter *= currentNumber;
   }
 
   return result;
