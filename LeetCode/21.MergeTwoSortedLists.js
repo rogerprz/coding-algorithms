578c0e607f7112fb9010857ab47c2bc5c6804048
Roger Perez <ropere@microsoft.com>
2023-07-21 15:03:50 -0700

    simplified version

--- LeetCode/21.MergeTwoSortedLists.js
+++ LeetCode/21.MergeTwoSortedLists.js
@@ -5,29 +5,25 @@ function ListNode(val, next) {
 }
 
 const mergeTwoLists = function (l1, l2) {
-  let list = new ListNode();
-  const head = list;
+  const list = new ListNode();
+  let tail = list;
 
-  while (l1 || l2) {
-    if (!l1 && l2) {
-      list.next = l2;
-      l2 = l2.next;
-    } else if (l1 && !l2) {
-      list.next = l1;
-      l1 = l1.next;
-    } else if (l1.val < l2.val) {
-      list.next = new ListNode(l1.val);
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      tail.next = l1;
       l1 = l1.next;
     } else {
-      list.next = new ListNode(l2.val);
+      tail.next = l2;
       l2 = l2.next;
     }
-    list = list.next;
+    tail = tail.next;
   }
 
-  return head.next;
-};
+  if (l1) tail.next = l1;
+  else if (l2) tail.next = l2;
 
+  return list.next;
+};
 const list1 = [1, 2, 4];
 const list2 = [1, 3, 4];
 
