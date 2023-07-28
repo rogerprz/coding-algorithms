4a87a4607051eee8787eefd87ae7b0d6f9a25a37
Roger Perez <ropere@microsoft.com>
2023-07-27 21:51:43 -0700

    is subsequence

---
+++ LeetCode/392.IsSubsequence.js
@@ -0,0 +1,25 @@
+const isSubsequence = function (s, t) {
+  // Not my solution
+  let left = 0;
+  let right = 0;
+
+  // Not my solution
+  // Not my solution
+
+  // Create a loop that checkes both lengths are greter than their pointers
+  while (left < s.length && right < t.length) {
+    const leftNum = s[left];
+    const rightNum = t[right];
+    // if lefNum === rightNum increase leftIndex
+    if (leftNum === rightNum) {
+      left++;
+    }
+    // increase longer string every loop
+
+    right++;
+  }
+
+  return left === s.length;
+};
+
+console.log(isSubsequence('abc', 'ahbgdc'));
