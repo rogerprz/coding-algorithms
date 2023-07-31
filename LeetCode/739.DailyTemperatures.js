fa45135d1f1045aa5446889dfbd25f1304aa2dd1
Roger Perez <ropere@microsoft.com>
2023-07-30 17:22:07 -0700

    third daily temps solutions

--- LeetCode/739.DailyTemperatures.js
+++ LeetCode/739.DailyTemperatures.js
@@ -59,6 +59,31 @@ const altDailyTemperatures = function (temps) {
   return results;
 };
 
+const thirdDailyTemperatures = function (temps) {
+  const results = Array(temps.length).fill(0);
+
+  let l = 0;
+  let r = 1;
+
+  while (l <= temps.length - 2) {
+    const leftTemp = temps[l];
+    const rightTemp = temps[r];
+    if (leftTemp < rightTemp) {
+      results[l] = r - l;
+      l++;
+      r = l + 1;
+    } else if (r === temps.length - 1) {
+      l++;
+      r = l + 1;
+    } else {
+      r++;
+    }
+  }
+
+  return results;
+};
+
 const temps = [73, 74, 75, 71, 69, 72, 76, 73];
 console.log(dailyTemperatures(temps));
 console.log(altDailyTemperatures(temps));
+console.log(thirdDailyTemperatures(temps));
