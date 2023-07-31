b4694445f94f06dc8010a4a2e58a40f92d26cded
Roger Perez <ropere@microsoft.com>
2023-07-30 17:11:08 -0700

    add alt sollution

--- LeetCode/739.DailyTemperatures.js
+++ LeetCode/739.DailyTemperatures.js
@@ -30,5 +30,35 @@ const dailyTemperatures = function (temps) {
   return results;
 };
 
+const altDailyTemperatures = function (temps) {
+  const results = Array(temps.length).fill(0);
+
+  let l = 0;
+  let r = 1;
+
+  let count = 0;
+  while (l <= temps.length - 2) {
+    const leftTemp = temps[l];
+    const rightTemp = temps[r];
+    if (leftTemp < rightTemp) {
+      count++;
+      results[l] = count;
+      count = 0;
+      l++;
+      r = l + 1;
+    } else if (r === temps.length - 1) {
+      count = 0;
+      l++;
+      r = l + 1;
+    } else {
+      count++;
+      r++;
+    }
+  }
+
+  return results;
+};
+
 const temps = [73, 74, 75, 71, 69, 72, 76, 73];
 console.log(dailyTemperatures(temps));
+console.log(altDailyTemperatures(temps));
