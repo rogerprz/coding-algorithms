79657372d5ac7d924302ae53f2904c6909432c6c
rogercodes1 <rogerperez.us@gmail.com>
2023-08-01 22:56:53 -0700

    704. Binary Search

---
+++ LeetCode/704.BinarySearch.js
@@ -0,0 +1,20 @@
+const search = function (nums, target) {
+  let l = 0;
+  let r = nums.length - 1;
+
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
+    let midNum = nums[mid];
+
+    if (midNum === target) return mid;
+
+    if (midNum < target) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+  return -1;
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9)); // 4
