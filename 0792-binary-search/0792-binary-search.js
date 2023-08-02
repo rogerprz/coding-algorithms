8b275fca424d6220ac9215da4eaffb085c654bed
Roger Perez <rogerprz@github.com>
2023-08-01 23:07:44 -0700

    Time: 53 ms (94.09%), Space: 45.5 MB (8.72%) - LeetHub

--- 0792-binary-search/0792-binary-search.js
+++ 0792-binary-search/0792-binary-search.js
@@ -12,11 +12,8 @@ var search = function(nums, target) {
     const mid = Math.floor((l + r)/ 2)
     
     if (nums[mid] === target) return mid
-    else if (nums[mid] < target) {
-      l = mid + 1
-    } else {
-      r = mid - 1
-    }
+    else if (nums[mid] < target) l = mid + 1 
+    else r = mid - 1
   }
   return -1
 };
