997599640078ba7e03eb5ac873c82e3af2199265
Roger Perez <ropere@microsoft.com>
2023-08-20 12:50:42 -0700

    add new solutions

---
+++ LeetCode/238.productExceptSelf.js
@@ -0,0 +1,22 @@
+const productExceptSelf = function (nums) {
+  const results = [];
+
+  let preNum = 1;
+  for (let i = 0; i < nums.length; i++) {
+    results[i] = 1;
+    results[i] *= preNum;
+    preNum *= nums[i];
+  }
+
+  let postNum = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    results[i] *= postNum;
+    postNum *= nums[i];
+  }
+
+  return results;
+};
+
+const case1 = [1, 2, 3, 4];
+console.log('HERE:');
+productExceptSelf(case1);
