562c668df5c997fae003f66ffbd14de76b697b0f
Roger Perez <ropere@microsoft.com>
2023-08-20 16:50:34 -0700

    Add fib

---
+++ YouTube/freeCodeCamp/fib.js
@@ -0,0 +1,12 @@
+const fib = (n) => {
+  if (n <= 2) return 1;
+
+  return fib(n - 1) + fib(n - 2);
+};
+
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
+
+// To Run:
+// node YouTube/freeCodeCamp/fib.js
