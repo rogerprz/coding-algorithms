334944da5053343eb8841c58c996f8d2e4fba4d5
Roger Perez <ropere@microsoft.com>
2023-08-20 16:40:44 -0700

    Add Diameter of Binary tree

---
+++ LeetCode/543.DiameterofBinaryTree.js
@@ -0,0 +1,20 @@
+const diameterOfBinaryTree = function (root) {
+  // Not my solution
+  let diameter = 0;
+
+  const maxDiameter = (node) => {
+    if (!node) return 0;
+
+    const leftMax = maxDiameter(node.left);
+    const rightMax = maxDiameter(node.right);
+
+    diameter = Math.max(diameter, leftMax + rightMax);
+
+    return 1 + Math.max(leftMax, rightMax);
+  };
+  maxDiameter(root);
+  return diameter;
+};
+
+const root = [1, 2, 3, 4, 5];
+console.log(diameterOfBinaryTree(root));
