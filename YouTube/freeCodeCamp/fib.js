508264dc63802f80182b2948e93d53433fb9feb8
Roger Perez <ropere@microsoft.com>
2023-08-24 21:36:47 -0700

    memo fib

--- YouTube/freeCodeCamp/fib.js
+++ YouTube/freeCodeCamp/fib.js
@@ -8,7 +8,20 @@ const fib = (n) => {
 
 console.log(fib(6));
 console.log(fib(7));
-console.log(fib(8));
+console.log(fib(50));
 
 // How to improve this solution?
 // Memoization
+
+// js object, keys will be arg to fn, value will be the return value
+const fibWithMemo = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
+
+  memo[n] = fibWithMemo(n - 1, memo) + fibWithMemo(n - 2, memo);
+  return memo[n];
+};
+console.log(fibWithMemo(6));
+console.log(fibWithMemo(7));
+console.log(fibWithMemo(8));
+console.log(fibWithMemo(50));
