7cbc5d4792707e40fbb0f561c365efa56d343fd9
Roger Perez <ropere@microsoft.com>
2023-09-21 23:42:55 -0700

    Add 724. Find Pivot Index

---
+++ LeetCode/724.FindPivotIndex.js
@@ -0,0 +1,16 @@
+const pivotIndex = function (nums) {
+  let rightSum = nums.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  let leftSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    const currentVal = nums[i];
+    rightSum -= currentVal;
+
+    if (leftSum === rightSum) return i;
+
+    leftSum += currentVal;
+  }
+  return -1;
+};
+
+console.log(pivotIndex([1, 7, 3, 6, 5, 6])); // 3
