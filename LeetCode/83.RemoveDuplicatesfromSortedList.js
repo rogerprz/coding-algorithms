3f8fc167f8450d83e0ce7cb06fdf0bf45014d4ba
Roger Perez <ropere@microsoft.com>
2023-10-15 22:57:29 -0700

    Remove duplicates

--- LeetCode/83.RemoveDuplicatesfromSortedList.js
+++ LeetCode/83.RemoveDuplicatesfromSortedList.js
@@ -18,6 +18,24 @@ const deleteDuplicates = function (head) {
   return head;
 };
 
+const deleteDuplicatesRecursion = function (head) {
+  const list = head;
+  const removeDupes = (list) => {
+    if (list && !list.next) return list;
+    if (!list) return list;
+
+    if (list.val === list.next.val) {
+      list.next = list.next.next;
+    } else {
+      list = list.next;
+    }
+    return removeDupes(list);
+  };
+  removeDupes(list);
+
+  return head;
+};
+
 // Sample Test case:
 const node = new ListNode(1);
 node.next = new ListNode(1);
@@ -25,3 +43,4 @@ node.next.next = new ListNode(2);
 node.next.next.next = new ListNode(3);
 
 console.log(deleteDuplicates(node));
+console.log(deleteDuplicatesRecursion(node));
