9e9ed347330b15236955e0acaaf552c36abef5ef
Roger Perez <ropere@microsoft.com>
2023-10-16 23:27:29 -0700

    Invert Binary tree

--- LeetCode/226.InvertBinaryTree.js
+++ LeetCode/226.InvertBinaryTree.js
@@ -1,14 +1,35 @@
+// Last solved: Oct 16, 2023
+// Previous Solved: Aug 3, 2023
+
 const invertTree = function (root) {
   if (!root) return root;
-
-  let tree = root;
-  let stack = [tree];
+  const tree = root;
+  const stack = [tree];
 
   while (stack.length > 0) {
     const node = stack.pop();
 
-    let leftTemp = node.left;
-    let rightTemp = node.right;
+    const tempLeft = node.left;
+
+    node.left = node.right;
+    node.right = tempLeft;
+
+    if (node.left) stack.push(node.left);
+    if (node.right) stack.push(node.right);
+  }
+  return tree;
+};
+const invertTreeOld = function (root) {
+  if (!root) return root;
+
+  const tree = root;
+  const stack = [tree];
+
+  while (stack.length > 0) {
+    const node = stack.pop();
+
+    const leftTemp = node.left;
+    const rightTemp = node.right;
 
     node.left = rightTemp;
     node.right = leftTemp;
@@ -28,3 +49,4 @@ const invertTree = function (root) {
 const root = [4, 2, 7, 1, 3, 6, 9];
 // Output: [4,7,2,9,6,3,1]
 invertTree(root);
+invertTreeOld(root);
