d17854e4be516374faf3d010c83e6d51e163a129
Roger Perez <rogerprz@github.com>
2023-10-21 15:16:24 -0700

    Time: 204 ms (78.99%), Space: 96.1 MB (64.71%) - LeetHub

---
+++ 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var removeNodes = function(head) {
+    // NOT MY SOLUTION
+    if (!head) return null
+    
+    head.next = removeNodes(head.next)
+
+    if (head.next && head.val < head.next.val) return head.next
+
+    return head
+};
