566011a90768343ff5490dcb87d3d4b6d6c4a5b8
Roger Perez <rogerprz@github.com>
2023-10-22 22:48:05 -0700

    Time: 53 ms (45.51%), Space: 41.8 MB (74.95%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
@@ -9,24 +9,25 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-
-//  We define the function to implement as swap(head), where the input parameter head refers to the head of a linked list. The function should return the head of the new linked list that has any adjacent nodes swapped.
-
-// Following the guidelines we lay out above, we can implement the function as follows:
-
-// First, we swap the first two nodes in the list, i.e. head and head.next;
-// Then, we call the function self as swap(head.next.next) to swap the rest of the list following the first two nodes.
-// Finally, we attach the returned head of the sub-list in step (2) with the two nodes swapped in step (1) to form a new linked list.
 var swapPairs = function(head) {
-    if (!head || !head.next) {
-        return head
+    if (!head) return head 
+
+    let dummyNode = new ListNode(0, head)
+    let prevNode = dummyNode
+
+
+    while (head && head.next) {
+
+        let firstNode = head
+        let secondNode = head.next
+
+        prevNode.next = secondNode
+        firstNode.next = secondNode.next
+        secondNode.next = firstNode
+
+        prevNode = firstNode
+        head = firstNode.next
     }
 
-    const firstNode = head 
-    const secondNode = head.next
-
-    firstNode.next = swapPairs(secondNode.next)
-    secondNode.next = firstNode
-
-    return secondNode
+    return dummyNode.next
 };
