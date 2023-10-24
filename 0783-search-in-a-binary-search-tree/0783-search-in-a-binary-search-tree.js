a3574f5bd23edaec931fbe5701101814c82e535f
Roger Perez <rogerprz@github.com>
2023-10-23 22:01:49 -0700

    Time: 73 ms (33.02%), Space: 49.2 MB (52.11%) - LeetHub

---
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    if(!root) return root
+    console.log('R:', root.val, 'VAL:', val)
+    if (root.val === val) return root
+
+
+
+
+    return val< root.val ? searchBST(root.left, val) : searchBST(root.right, val)
+};
