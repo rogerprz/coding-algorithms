afd8058c3b2ae8f0ca30edadb504a49cb93d7c53
Roger Perez <rogerprz@github.com>
2023-10-31 22:16:54 -0700

    Time: 153 ms (12.72%), Space: 54.1 MB (42.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,27 +3,23 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-// Input: strs = ["eat","tea","tan","ate","nat","bat"]
-// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
-// if strs len === 1 then we return an array of arrays with that single elem. 
-if (strs.length === 1) return [strs]
-// array of words, where we want to group the words together if they have the same wording.
-// create a hash where the key is a sorted word, ex: eat => aet, {aet: [eat]}
-const map = {}
-// loop through the array
-    for (let i = 0; i<strs.length; i++){
-        // split word, sort word, 
-        const word = strs[i]
-        const sortedWord = word.split("").sort().join('')
-        // if word in hash then we push it to array
-        if (sortedWord in map){
-            map[sortedWord].push(word)
-        } else {
-        // else we create key and new array. push word
-            map[sortedWord] = [word]
-        }
-    }
-// return the values as an array
-return Object.values(map)
+    if (strs.length <=1) return [strs]
+    const hash = {}
 
+    for (let i = 0; i <strs.length; i++) {
+        const currElem = strs[i]
+    
+        const sortedElem = currElem.split("").sort()
+        const joinElem = sortedElem.join('')
+
+        if (joinElem in hash){
+            hash[joinElem].push(currElem)
+        }  else {
+            hash[joinElem] = [currElem]
+        }
+        // }
+      
+    }
+    console.log("VAL:", Object.values(hash))
+    return Object.values(hash)
 };
