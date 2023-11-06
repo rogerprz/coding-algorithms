8e478b55ebb717e0ddd0954607926ffb18ad4a33
Roger Perez <rogerprz@github.com>
2023-11-05 20:32:36 -0800

    Time: 46 ms (85.37%), Space: 42.4 MB (22.32%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
@@ -10,15 +10,22 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-    if (!head || !head.next) {
-        return head 
+    if (!head) return head 
+    // head = [1,2,3,4]
+    // Output: [2,1,4,3]
+    let dummyNode = new ListNode(0, head) // [0, 1,2,3,4]
+    let prev = dummyNode // [0, 1,2,3,4]
+
+    while (prev.next && prev.next.next) {
+        let secondNode = prev.next  // [1, 2,3,4]
+        let firstNode = prev.next.next // 2 [2,3,4]
+
+        prev.next = firstNode  // [0, 1,2,3,4] -> [0,2,3,4]
+        secondNode.next = firstNode.next // [1, 2,3,4] -> [1, 3,4]
+        firstNode.next = secondNode     //  [2,3,4] -> [2, 3,4]
+
+        prev = prev.next.next
+
     }
-
-    let firstNode = head
-    let secondNode = head.next
-
-    firstNode.next = swapPairs(secondNode.next)
-    secondNode.next = firstNode
-
-    return secondNode
+    return dummyNode.next
 };
