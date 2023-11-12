8526df6b106344d5e95dbfcbfd015666c468788c
Roger Perez <rogerprz@github.com>
2023-11-11 17:39:07 -0800

    Time: 223 ms (59.39%), Space: 72.7 MB (27.03%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -3,39 +3,18 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temps) {
-    const results =[]
+    let result = Array(temps.length).fill(0)
+    let stack = []
 
-    let l = 0 
-    let r = 1
+    for (let i = 0; i<temps.length; i++) {
+        let currTemp = temps[i]
+        while (stack.length > 0 && currTemp > stack[stack.length-1][0]) {
+            const [temp, index] = stack.pop()
+            result[index] = i - index
+        }
+        stack.push([currTemp, i])
 
-    let count = 0 
-    while (l <= temps.length-2) {
-        const leftTemp = temps[l]
-        const rightTemp = temps[r]
-        // console.log('----------------')
-        // console.log( temps)
-        // console.log('L',l, 'R:', r, 'COUNT:', count)
-        // console.log(leftTemp, rightTemp, results)
-        if (leftTemp < rightTemp) {
-            count++
-            results[l] = count
-            count = 0
-            l++
-            r = l+1
-        } 
-        else if (r === temps.length-1) {
-            count = 0 
-            results[l] = 0
-            l++
-            r = l+1
-        }
-        else {
-            count++
-            r++
-        }
     }
 
-    results.push(0)
-
-    return results
+    return result
 };
