f4377912ab9b786ab5c58d2005ace0920f14004a
Roger Perez <ropere@microsoft.com>
2023-11-13 22:28:31 -0800

    Connected Component count

--- Graphs/connectedComponentsCount.js
+++ Graphs/connectedComponentsCount.js
@@ -2,6 +2,86 @@ const connectedComponentsCount = (graph) => {
   const visited = new Set();
   let count = 0;
 
+  const explore = (graph, current, visited) => {
+    if (visited.has(current)) return false;
+    visited.add(current);
+    for (const neighbor of graph[current]) {
+      explore(graph, neighbor, visited);
+    }
+
+    return true;
+  };
+
+  for (const neighbors in graph) {
+    console.log(neighbors);
+
+    if (explore(graph, parseInt(neighbors), visited)) {
+      count++;
+    }
+  }
+  return count;
+};
+
+const graph = {
+  1: [2],
+  2: [1],
+  3: [],
+  4: [6],
+  5: [6],
+  6: [4, 5, 7, 8],
+  7: [6],
+  8: [6]
+};
+console.log(connectedComponentsCount(graph)); // count = 3
+
+//
+
+//
+//
+//
+//
+//
+//
+//
+//
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+////
+//
+
+const connectedComponentsCountAnswer = (graph) => {
+  const visited = new Set();
+  let count = 0;
+
   for (const node in graph) {
     if (explore(graph, parseInt(node), visited)) {
       count++;
@@ -19,15 +99,3 @@ const explore = (graph, current, visited) => {
   }
   return true;
 };
-
-const graph = {
-  1: [2],
-  2: [1],
-  3: [],
-  4: [6],
-  5: [6],
-  6: [4, 5, 7, 8],
-  7: [6],
-  8: [6]
-};
-console.log(connectedComponentsCount(graph)); // count = 3
