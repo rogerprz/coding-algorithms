7194d3885a7d7eb43e102279e9f7bf397a90fbf1
Roger Perez <ropere@microsoft.com>
2023-11-13 23:02:00 -0800

    add more examples

--- Graphs/largestComponent.js
+++ Graphs/largestComponent.js
@@ -16,7 +16,7 @@ const largestComponent = (graph) => {
     return true;
   };
   for (const node in graph) {
-    if (traverse(graph, parseInt(node), visited)) {
+    if (traverse(graph, node, visited)) {
       if (count > max) max = count;
     }
     count = 0;
@@ -36,3 +36,42 @@ const graph = {
 };
 
 console.log(largestComponent(graph));
+
+console.log(
+  'OUTPUTis 4: ',
+  largestComponent({
+    0: ['8', '1', '5'],
+    1: ['0'],
+    5: ['0', '8'],
+    8: ['0', '5'],
+    2: ['3', '4'],
+    3: ['2', '4'],
+    4: ['3', '2']
+  })
+);
+
+console.log(
+  'OUTPUTis 5: ',
+  largestComponent({
+    3: [],
+    4: ['6'],
+    6: ['4', '5', '7', '8'],
+    8: ['6'],
+    7: ['6'],
+    5: ['6'],
+    1: ['2'],
+    2: ['1']
+  })
+);
+
+console.log(
+  'OUTPUTis 6: ',
+  largestComponent({
+    1: ['2'],
+    2: ['1', '8'],
+    6: ['7'],
+    9: ['8'],
+    7: ['6', '8'],
+    8: ['9', '7', '2']
+  })
+);
