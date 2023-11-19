55b8a714c56d7e173e92f53c4652e22a39a95275
Roger Perez <ropere@microsoft.com>
2023-11-19 14:56:36 -0800

    solution b4 cleanup

--- Graphs/shortestPath.js
+++ Graphs/shortestPath.js
@@ -14,7 +14,31 @@ const getGraph = (edges) => {
   return hash;
 };
 const shortestPath = (edges, nodeA, nodeB) => {
+  console.log('-------------START----------------');
   const graph = getGraph(edges);
+  const visited = new Set(nodeA);
+  const queue = [[nodeA, 0]];
+  let shortest = -1;
+  console.log('NODE:', nodeA, nodeB, 'Q:', queue);
+  console.log('G:', graph);
+
+  while (queue.length > 0) {
+    console.log('QUEUE:', queue);
+    let [current, pos] = queue.shift();
+    console.log('N:', current, pos);
+    // const [leftNode, rightNode] = graph[node];
+    pos++;
+    for (const node of graph[current]) {
+      if (visited.has(node)) continue;
+      if (node === nodeB) {
+        shortest = pos;
+        return shortest;
+      }
+      visited.add(node);
+      queue.push([node, pos]);
+    }
+  }
+  return shortest;
 };
 
 console.log(
@@ -32,22 +56,22 @@ console.log(
   )
 ); // -> 2
 
-// console.log(
-//   'OUTPUT IS 3: ',
-//   shortestPath(
-//     [
-//       ['a', 'c'],
-//       ['a', 'b'],
-//       ['c', 'b'],
-//       ['c', 'd'],
-//       ['b', 'd'],
-//       ['e', 'd'],
-//       ['g', 'f']
-//     ],
-//     'a',
-//     'e'
-//   )
-// );
+console.log(
+  'OUTPUT IS 3: ',
+  shortestPath(
+    [
+      ['a', 'c'],
+      ['a', 'b'],
+      ['c', 'b'],
+      ['c', 'd'],
+      ['b', 'd'],
+      ['e', 'd'],
+      ['g', 'f']
+    ],
+    'a',
+    'e'
+  )
+);
 
 // console.log(
 //   'OUTPUT IS -1: ',
