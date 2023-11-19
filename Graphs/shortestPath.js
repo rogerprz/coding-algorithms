8329e0e32b606c5a4508ca3061ce65e51dfb38c6
Roger Perez <ropere@microsoft.com>
2023-11-19 14:19:39 -0800

    add getGraph fn

--- Graphs/shortestPath.js
+++ Graphs/shortestPath.js
@@ -1,8 +1,24 @@
-const shortestPath = (edges, nodeA, nodeB) => {};
+const getGraph = (edges) => {
+  const hash = {};
+
+  for (const [left, right] of edges) {
+    if (!(left in hash)) {
+      hash[left] = [];
+    }
+    if (!(right in hash)) {
+      hash[right] = [];
+    }
+    hash[left].push(right);
+    hash[right].push(left);
+  }
+  return hash;
+};
+const shortestPath = (edges, nodeA, nodeB) => {
+  const graph = getGraph(edges);
+};
 
 console.log(
   'OUTPUT IS 2: ',
-
   shortestPath(
     [
       ['w', 'x'],
@@ -16,36 +32,36 @@ console.log(
   )
 ); // -> 2
 
-console.log(
-  'OUTPUT IS 3: ',
-  shortestPath(
-    [
-      ['a', 'c'],
-      ['a', 'b'],
-      ['c', 'b'],
-      ['c', 'd'],
-      ['b', 'd'],
-      ['e', 'd'],
-      ['g', 'f']
-    ],
-    'a',
-    'e'
-  )
-);
+// console.log(
+//   'OUTPUT IS 3: ',
+//   shortestPath(
+//     [
+//       ['a', 'c'],
+//       ['a', 'b'],
+//       ['c', 'b'],
+//       ['c', 'd'],
+//       ['b', 'd'],
+//       ['e', 'd'],
+//       ['g', 'f']
+//     ],
+//     'a',
+//     'e'
+//   )
+// );
 
-console.log(
-  'OUTPUT IS -1: ',
-  shortestPath(
-    [
-      ['a', 'c'],
-      ['a', 'b'],
-      ['c', 'b'],
-      ['c', 'd'],
-      ['b', 'd'],
-      ['e', 'd'],
-      ['g', 'f']
-    ],
-    'b',
-    'g'
-  )
-);
+// console.log(
+//   'OUTPUT IS -1: ',
+//   shortestPath(
+//     [
+//       ['a', 'c'],
+//       ['a', 'b'],
+//       ['c', 'b'],
+//       ['c', 'd'],
+//       ['b', 'd'],
+//       ['e', 'd'],
+//       ['g', 'f']
+//     ],
+//     'b',
+//     'g'
+//   )
+// );
