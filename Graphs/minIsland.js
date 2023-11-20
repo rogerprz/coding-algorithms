0773bd8859a01681bac21c7bac6196f2f4912ff4
Roger Perez <ropere@microsoft.com>
2023-11-20 09:55:13 -0800

    cleanup

--- Graphs/minIsland.js
+++ Graphs/minIsland.js
@@ -18,18 +18,10 @@ const explore = (grid, row, col, visited) => {
   console.log('COL:', col, row, grid.length);
   const colInbounds = col >= 0 && col < grid[0].length;
 
-  if (!rowInbounds || !colInbounds) {
-    return 0;
-  }
-  if (grid[row][col] === 'W') {
-    console.log('WATER');
-    return 0;
-  }
+  if (!rowInbounds || !colInbounds) return 0;
+  if (grid[row][col] === 'W') return 0;
   const pos = `${row},${col}`;
-  if (visited.has(pos)) {
-    console.log('HAS');
-    return 0;
-  }
+  if (visited.has(pos)) return 0;
 
   visited.add(pos);
   let size = 1;
