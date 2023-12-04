be5cf6d437be45e7dd33aeafb2ffde7cd889d7c1
Roger Perez <ropere@microsoft.com>
2023-12-03 16:40:23 -0800

    fix canSum

--- YouTube/freeCodeCamp/canSum.js
+++ YouTube/freeCodeCamp/canSum.js
@@ -1,6 +1,22 @@
-const canSum = (targetSum, numbers) => {
-  console.log(numbers);
-  return targetSum;
+const canSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return true;
+  if (targetSum < 0) return false;
+
+  for (const num of numbers) {
+    const remainder = targetSum - num;
+    if (canSum(remainder, numbers, memo)) {
+      memo[targetSum] = true;
+      return true;
+    }
+  }
+  memo[targetSum] = false;
+  return false;
 };
 
-console.log(canSum(7, [5, 3, 4, 7]));
+console.log('Output: True', canSum(7, [2, 3])); // True
+console.log('Output: True:', canSum(7, [5, 3, 4, 7])); // true
+console.log('Output: False', canSum(7, [2, 4])); // false
+console.log('Output: True', canSum(8, [2, 3, 5])); // false
+console.log('Output: 300 False', canSum(300, [7, 14])); // false
+console.log('Output: 210 True', canSum(210, [7, 14])); // True
