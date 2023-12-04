57d8384af4ddb95c5022312e2fdca1a3b1b53884
Roger Perez <ropere@microsoft.com>
2023-12-03 16:12:46 -0800

    add solutions

---
+++ TechQuestions/burstBalloons.js
@@ -0,0 +1,5 @@
+// You are selected to play a game with n balloons. Each balloon is painted with a number on it representing the score you can get by popping the balloon. But there is a rule that you can not pop adjacent balloons.
+
+// Given an integer array nums representing the scores of each balloon, return the maximum score you can get without violating the rule.
+
+// [ 1, 4, 10, 5 , 2, 12, 3 ]
