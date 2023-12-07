69f46eca184a618e53e703bbf3855aaba796c886
Roger Perez <ropere@microsoft.com>
2023-12-07 11:35:16 -0800

    add description

---
+++ Structy/countConstruct.js
@@ -0,0 +1,4 @@
+// Write a function that accepts a target string and an array of strings;
+// The fn should return the number of ways that the "target" can be constructed by concatenating
+// elements of the "wordBank" array.
+// You can reuse elements of "wordBank" as many time as needed.
