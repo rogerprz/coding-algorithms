6c5dd364561c8f31df89302be80b764772daad01
Roger Perez <ropere@microsoft.com>
2023-12-06 22:08:01 -0800

    solved-ish

--- Structy/bestSum.js
+++ Structy/bestSum.js
@@ -3,7 +3,6 @@
 // If there is a tie for the shortest combo, you may return any of one of the shortest
 
 const bestSum = (targetSum, numbers, memo = {}) => {
-  if (targetSum in memo) return memo[targetSum];
   if (targetSum === 0) return [];
   if (targetSum < 0) return null;
 
@@ -11,19 +10,19 @@ const bestSum = (targetSum, numbers, memo = {}) => {
 
   for (const num of numbers) {
     const remainder = targetSum - num;
-    const remainCombo = bestSum(remainder, numbers, memo);
+    const currentCombo = bestSum(remainder, numbers, memo);
 
-    if (remainCombo !== null) {
-      const combo = [...remainCombo, num];
-      if (!shortestCombo || combo.length < shortestCombo.length) {
-        shortestCombo = combo;
+    if (currentCombo !== null) {
+      const updateCombo = [...currentCombo, num];
+      if (!shortestCombo || updateCombo.length < shortestCombo.length) {
+        shortestCombo = updateCombo;
       }
     }
   }
-  memo[targetSum] = shortestCombo;
+
   return shortestCombo;
 };
 console.log('Output: 7', bestSum(7, [5, 4, 7, 3])); // [7]
-console.log('Output: 3/5', bestSum(8, [5, 2, 3])); // [3,5]
-console.log('Output: 3/5', bestSum(8, [1, 4, 5])); // [4,4]
+console.log('Output: [3,5]', bestSum(8, [5, 2, 3])); // [3,5]
+console.log('Output: [4,4]', bestSum(8, [1, 4, 5])); // [4,4]
 console.log('Output: [25x4]', bestSum(100, [1, 2, 5, 25])); // [25, 25, 25, 25]
