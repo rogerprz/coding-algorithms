7cb3e795856b57e84fe28174deb7b61a67c1905c
Roger Perez <ropere@microsoft.com>
2023-12-07 12:05:53 -0800

    optimized

--- Structy/countConstruct.js
+++ Structy/countConstruct.js
@@ -3,20 +3,25 @@
 // elements of the "wordBank" array.
 // You can reuse elements of "wordBank" as many time as needed.
 
-const countConstruct = (target, wordBank) => {
+const countConstruct = (target, wordBank, memo = {}) => {
+  if (target in memo) return memo[target];
   if (target === '') return 1;
   // if (target )
 
   let totalCount = 0;
   for (const word of wordBank) {
     if (target.indexOf(word) === 0) {
-      const numOfWaysForRest = countConstruct(target.splice(word.length), wordBank);
+      const numOfWaysForRest = countConstruct(target.slice(word.length), wordBank, memo);
       totalCount += numOfWaysForRest;
     }
   }
+  memo[target] = totalCount;
   return totalCount;
 };
 
 console.log('Result: 2: ', countConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']));
 console.log('Result: 1: ', countConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']));
-console.log('Result: 0: ', countConstruct('eeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eeee', 'eee', 'eeeeeee']));
+console.log(
+  'Result: 0: ',
+  countConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eeee', 'eee', 'eeeeeee'])
+);
