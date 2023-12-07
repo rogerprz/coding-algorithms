ee51c04f0ebfc27463a447ad41b8037b1ef66b4e
Roger Perez <ropere@microsoft.com>
2023-12-07 11:40:14 -0800

    add initial

--- Structy/countConstruct.js
+++ Structy/countConstruct.js
@@ -2,3 +2,9 @@
 // The fn should return the number of ways that the "target" can be constructed by concatenating
 // elements of the "wordBank" array.
 // You can reuse elements of "wordBank" as many time as needed.
+
+const countConstruct = (target, wordBank) => {
+  return target;
+};
+
+console.log('Result: 1: ', countConstruct('abcdef', []));
