f7735ba2ce787acf6f21c1560e2b409ff5aabfbc
Roger Perez <ropere@microsoft.com>
2023-12-08 14:43:07 -0800

    solved allConstruct

--- Structy/allConstruct.js
+++ Structy/allConstruct.js
@@ -2,19 +2,30 @@
 // The fn should return a 2D array containing all of the ways that the "target" can be constructed
 // by concatenating elements of the the "wordBank" array.
 
-const allConstruct = (target, wordBank) => {
+const allConstruct = (target, wordBank, memo = {}) => {
+  if (target in memo) return memo[target];
+  if (target === '') return [[]];
+
+  const result = [];
+  for (const word of wordBank) {
+    if (target.indexOf(word) === 0) {
+      const suffix = target.slice(word.length);
+      const suffixWays = allConstruct(suffix, wordBank);
+
+      const targetWays = suffixWays.map((way) => [word, ...way]);
+      result.push(...targetWays);
+    }
+  }
   // test
-  console.log('T:', target);
-  return [target, wordBank];
+  //   console.log('T:', target);
+  memo[target] = result;
+  return result;
 };
 
-console.log('Result: true: ', allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c']));
-console.log('Result: false:', allConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']));
-console.log('Result: true: ', allConstruct('', ['cat', 'dog', 'mouse']));
-console.log('Result: True: ', allConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']));
-
 console.log('Result: 2 ways: ', allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']));
-console.log(
-  'Result: false:',
-  allConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eeee', 'eee', 'eeeeeee'])
-);
+console.log('Result: 4 ways:', allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c']));
+console.log('Result: 0 ways:', allConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']));
+console.log('Result: 0 ways: ', allConstruct('', ['cat', 'dog', 'mouse']));
+console.log('Result: 4 ways: ', allConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']));
+
+console.log('Result: 0 ways:', allConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['ee', 'eeee', 'eee', 'eeeeeee']));
