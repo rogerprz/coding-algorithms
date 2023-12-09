837bb53d7c7540204de58df759f8a09084b93b0c
Roger Perez <ropere@microsoft.com>
2023-12-09 12:31:50 -0800

    solved gTT

--- Structy/dp-tabulation/gridTravelerTabulation.js
+++ Structy/dp-tabulation/gridTravelerTabulation.js
@@ -3,11 +3,20 @@ const gridTravelerTabulation = (m, n) => {
     .fill()
     .map(() => Array(n + 1).fill(0));
   table[1][1] = 1;
-  console.log('T:', table);
+
+  for (let row = 0; row <= m; row++) {
+    for (let col = 0; col <= n; col++) {
+      const current = table[row][col];
+      if (col + 1 <= n) table[row][col + 1] += current;
+      if (row + 1 <= m) table[row + 1][col] += current;
+    }
+  }
+
+  return table[m][n];
 };
 
-// console.log(gridTravelerTabulation(1, 1)); // 1
+console.log(gridTravelerTabulation(1, 1)); // 1
 console.log(gridTravelerTabulation(2, 3)); // 3
-// console.log(gridTravelerTabulation(3, 2)); // 3
-// console.log(gridTravelerTabulation(3, 3)); // 6
-// console.log(gridTravelerTabulation(18, 18)); // 2333606220
+console.log(gridTravelerTabulation(3, 2)); // 3
+console.log(gridTravelerTabulation(3, 3)); // 6
+console.log(gridTravelerTabulation(18, 18)); // 2333606220
