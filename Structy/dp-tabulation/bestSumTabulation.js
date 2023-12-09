bf50d08bcd20ffb7bfda73229662cb13dc522b7b
Roger Perez <ropere@microsoft.com>
2023-12-09 13:23:55 -0800

    add boiler plate for problem bestSum

---
+++ Structy/dp-tabulation/bestSumTabulation.js
@@ -0,0 +1,9 @@
+// Write a fn "bestSum(targetSum, numbers) that takes in a targetSum and an array of numbers as args.
+// The fn should return an array containing the shortest combination of numbers that add up to exactly the targetSum.
+// If there is a tie for the shortest combination, you may return any one of the shortest.
+
+const bestSum = (targetSum, numbers) => {};
+console.log('Output: 7', bestSum(7, [5, 4, 7, 3])); // [7]
+console.log('Output: [3,5]', bestSum(8, [5, 2, 3])); // [3,5]
+console.log('Output: [4,4]', bestSum(8, [1, 4, 5])); // [4,4]
+console.log('Output: [25x4]', bestSum(100, [1, 2, 5, 25])); // [25, 25, 25, 25]
