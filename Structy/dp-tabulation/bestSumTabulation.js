7d13488b7d5fe60e94ec60a897c2a8c9b7095c4c
Roger Perez <ropere@microsoft.com>
2023-12-09 14:32:31 -0800

    solved bestSUm Tab

--- Structy/dp-tabulation/bestSumTabulation.js
+++ Structy/dp-tabulation/bestSumTabulation.js
@@ -2,7 +2,25 @@
 // The fn should return an array containing the shortest combination of numbers that add up to exactly the targetSum.
 // If there is a tie for the shortest combination, you may return any one of the shortest.
 
-const bestSum = (targetSum, numbers) => {};
+const bestSum = (targetSum, numbers) => {
+  const table = Array(targetSum + 1).fill(null);
+  table[0] = [];
+  //   console.log('T:', table);
+  for (let i = 0; i <= targetSum; i++) {
+    if (table[i] !== null) {
+      for (const num of numbers) {
+        const combination = [...table[i], num];
+
+        if (!table[i + num] || table[i + num].length > combination.length) {
+          table[i + num] = combination;
+        }
+      }
+    }
+  }
+  //   console.log('T:', table);
+
+  return table[targetSum];
+};
 console.log('Output: 7', bestSum(7, [5, 4, 7, 3])); // [7]
 console.log('Output: [3,5]', bestSum(8, [5, 2, 3])); // [3,5]
 console.log('Output: [4,4]', bestSum(8, [1, 4, 5])); // [4,4]
