050eb6fc8db34b85bf5fe7ad3d8566f6f7ef2ab3
Roger Perez <ropere@microsoft.com>
2023-12-09 13:19:49 -0800

    my solution for howSumTabulation

--- Structy/dp-tabulation/howSumTabulation.js
+++ Structy/dp-tabulation/howSumTabulation.js
@@ -3,7 +3,24 @@
 // If there is no combination that adds up to the targetSum, then return null.
 
 const howSumTabulation = (targetSum, numbers) => {
-  const table = Array(targetSum + 1).fill(0);
+  const table = Array(targetSum + 1).fill(null);
+  table[0] = [];
+
+  for (let i = 0; i < targetSum; i++) {
+    const current = table[i];
+    if (current !== null) {
+      for (const num of numbers) {
+        if (num + i > targetSum) continue;
+        if (table[i + num] === null) {
+          table[i + num] = [num];
+        } else {
+          table[i + num] = [...table[i], num];
+        }
+      }
+    }
+  }
+
+  return table[targetSum];
 };
 
 console.log('Output: [3,2,2]', howSumTabulation(7, [2, 3])); // True
