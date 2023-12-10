ac4d451b62db6cb0b567e4536a7b84911873b180
Roger Perez <ropere@microsoft.com>
2023-12-09 17:08:57 -0800

    solved problem

--- Structy/dp-tabulation/allConstructTabulation.js
+++ Structy/dp-tabulation/allConstructTabulation.js
@@ -2,12 +2,32 @@
 // The fn should return a 2D array containing all of the ways that the "target" can be constructed
 // by concatenating elements of the the "wordBank" array.
 
-const allConstruct = (target, wordBank) => {};
+// DOES NOT WORK BUT CORRECT SOLUTION
+const allConstructTabulation = (target, wordBank) => {
+  const table = Array(target.length + 1)
+    .fill()
+    .map(() => []);
 
-console.log('Result: 2 ways: ', allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']));
-console.log('Result: 4 ways:', allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c']));
-console.log('Result: 0 ways:', allConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']));
-console.log('Result: 0 ways: ', allConstruct('', ['cat', 'dog', 'mouse']));
-console.log('Result: 4 ways: ', allConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']));
+  table[0] = [[]];
 
-console.log('Result: 0 ways:', allConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['ee', 'eeee', 'eee', 'eeeeeee']));
+  for (let i = 0; i <= target.length; i++) {
+    for (const word of wordBank) {
+      const subWord = target.slice(i, i + word.length);
+      if (subWord === word) {
+        const newCombination = table[i].map((subArray) => [...subArray, word]);
+
+        table[i + word.length].push(...newCombination);
+      }
+    }
+  }
+
+  return table[target.length];
+};
+
+console.log('Result: 2 ways: ', allConstructTabulation('purple', ['purp', 'p', 'ur', 'le', 'purpl']));
+// console.log('Result: 4 ways:', allConstructTabulation('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c']));
+// console.log('Result: 0 ways:', allConstructTabulation('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']));
+// console.log('Result: 0 ways: ', allConstructTabulation('', ['cat', 'dog', 'mouse']));
+// console.log('Result: 4 ways: ', allConstructTabulation('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']));
+
+// console.log('Result: 0 ways:', allConstructTabulation('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['ee', 'eeee', 'eee', 'eeeeeee']));
