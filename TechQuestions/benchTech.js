a1521fb81859294f8f51a583b048dda651b6eba2
Roger Perez <ropere@microsoft.com>
2024-04-05 00:15:10 -0700

    remove incorrect code

--- TechQuestions/benchTech.js
+++ TechQuestions/benchTech.js
@@ -58,26 +58,3 @@ function findProteins(genes) {
 const result = findProteins(exampleGenes);
 console.log(result);
 console.log(result.length === expectedResult.length); // Output: true
-
-// Current output, incorrect:
-// [
-//   { name: 'DBZ', startIndex: 0, endIndex: 7 },
-//   { name: 'PEPPY', startIndex: 4, endIndex: 11 },
-//   { name: 'cryNow', startIndex: 7, endIndex: 16 },
-//   { name: 'DBZ_cryNow', startIndex: 0, endIndex: 16 },
-//   { name: 'bro2', startIndex: 7, endIndex: 22 },
-//   { name: 'DBZ_bro2', startIndex: 0, endIndex: 22 },
-//   { name: 'cr8', startIndex: 11, endIndex: 22 },
-//   { name: 'PEPPY_cr8', startIndex: 4, endIndex: 22 },
-//   { name: 'DBZ', startIndex: 15, endIndex: 22 },
-//   { name: 'PS', startIndex: 22, endIndex: 28 },
-//   { name: 'DBZ_PS', startIndex: 15, endIndex: 28 },
-//   { name: 'bro2_DBZ_PS', startIndex: 7, endIndex: 28 },
-//   { name: 'DBZ_bro2_bro2_DBZ_PS', startIndex: 7, endIndex: 28 },
-//   { name: 'cr8_DBZ_bro2_bro2_DBZ_PS', startIndex: 11, endIndex: 28 },
-//   {
-//     name: 'PEPPY_cr8_cr8_DBZ_bro2_bro2_DBZ_PS',
-//     startIndex: 11,
-//     endIndex: 28
-//   }
-// ];
