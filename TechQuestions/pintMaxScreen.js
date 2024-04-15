af448d4b4311cd135f71af20700305c5d71f2d13
Roger Perez <ropere@microsoft.com>
2024-04-15 16:05:55 -0700

    fix

--- TechQuestions/pintMaxScreen.js
+++ TechQuestions/pintMaxScreen.js
@@ -4,46 +4,46 @@
 
 // pinHeights = [3, 5, 7]
 
-// maxScreenHeight = 4 → return -1
+// maxHeight = 4 → return -1
 // (cannot fit the screen size)
 
-// maxScreenHeight = 10 → return 2
+// maxHeight = 10 → return 2
 // (3+7 would be the chosen pin heights)
 
-// maxScreenHeight  = 14 → return 2
+// maxHeight  = 14 → return 2
 // (7+7 would be the chosen pin heights)
 
-function minScreens(pinHeights, maxScreenHeight) {
+function minScreens(pinHeights, maxHeight) {
   console.log('Start');
   // const count = Infinity;
   let min = Infinity;
-  const explore = (pinHeights, maxScreenHeight, count) => {
+  const explore = (pinHeights, maxHeight, count) => {
     // [12, 5, 1]
     // base case
-    if (maxScreenHeight === 0) {
+    if (maxHeight === 0) {
       // console.log("REturn:", count)
       return count;
     }
-    if (maxScreenHeight < 0) {
+    if (maxHeight < 0) {
       return null;
     }
 
     for (let i = 0; i < pinHeights.length; i++) {
       const height = pinHeights[i];
-      explore(pinHeights, maxScreenHeight - height, count++);
+      explore(pinHeights, maxHeight - height, count++);
     }
     // console.log("LAS:", count)
     return count;
   };
-  return explore(pinHeights, maxScreenHeight, 0);
+  return explore(pinHeights, maxHeight, 0);
   // return count
 }
 const pinHeights = [7, 5, 3];
-const maxScreenHeight = 10;
+const maxHeight = 10;
 
-// console.log(minScreens(pinHeights, maxScreenHeight))
+// console.log(minScreens(pinHeights, maxHeight))
 console.log(minScreens(pinHeights, 4)); // -1
-console.log(minScreens(pinHeights, 10)); // 2
+console.log(minScreens(pinHeights, maxHeight)); // 2
 console.log(minScreens([3, 6, 7], 5)); // 2
 console.log(minScreens([12, 5, 1], 25)); // 3, 12+12+1
 console.log(minScreens([7, 6, 2], 15)); // 3
@@ -62,17 +62,17 @@ console.log(minScreens([7, 6, 2], 15)); // 3
 //   let foundAnswer = false
 //   for (let i = 0; i < sorted.length; i++) {
 //     const num = sorted[i]
-//     if (maxScreenHeight < num ) continue
-//     if (total === maxScreenHeight) return count
-//     const remainder = maxScreenHeight % num
+//     if (maxHeight < num ) continue
+//     if (total === maxHeight) return count
+//     const remainder = maxHeight % num
 
 //     if (remainder % num === 0) {
-//       min = Math.min(Math.floor(maxScreenHeight/num), min)
+//       min = Math.min(Math.floor(maxHeight/num), min)
 //       foundAnswer = true
 //     } else {
 //       total += num
 //       count++
 //     }
 //   }
-//   if (total === maxScreenHeight) return count
+//   if (total === maxHeight) return count
 //   return foundAnswer ? min : -1
