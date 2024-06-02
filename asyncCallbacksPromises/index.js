5bff25d1b29e9e7705a3cf793b97e111b10e0a18
Roger Perez <ropere@microsoft.com>
2024-06-02 13:47:05 -0700

    promises chaining

---
+++ asyncCallbacksPromises/index.js
@@ -0,0 +1,44 @@
+// Testing for promises
+// const GITHUB_API = 'https://api.github.com/users/akshaymarch7';
+
+// const user = fetch(GITHUB_API);
+// console.log(user);
+// user.then((response) => {
+//   console.log('Response:', response);
+//   return response.json();
+// });
+
+const cart = ['shoes', 'shirts', 'pants'];
+
+const promise = createOrder(cart);
+
+promise
+  .then((orderId) => {
+    console.log('Order created:', orderId);
+  })
+  .catch((err) => {
+    console.log('Error:', err.message);
+  });
+
+// PRoducer
+function createOrder(cart) {
+  const pr = new Promise((resolve, reject) => {
+    // createOrder
+    // validate card
+    if (!validateCart(cart)) {
+      const err = new Error('Card is invalid');
+      reject(err);
+    }
+    const orderId = '1234';
+    if (orderId) {
+      setTimeout(() => {
+        resolve({ orderId });
+      }, 5000);
+    }
+  });
+  return pr;
+}
+
+function validateCart(cart) {
+  return false;
+}
