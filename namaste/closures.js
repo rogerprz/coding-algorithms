6b6e6b64b272f4441720d0b10e90f0efc3b6009e
Roger Perez <ropere@microsoft.com>
2024-06-16 16:16:23 -0700

    add examples

--- namaste/closures.js
+++ namaste/closures.js
@@ -1,8 +1,34 @@
-function x() {
-  var a = 7;
-  function y() {
-    console.log(a);
+// function x() {
+//   var a = 7;
+//   function y() {
+//     console.log(a);
+//   }
+//   return y;
+// }
+// var z = x();
+// console.log(z);
+// z();
+
+// A closure is a function along with it's a lexical scope is called a closure.
+// Uses for closures:
+// 1. Module Design Pattern
+// 2. Currying
+// 3. Function like once
+// 4. Memoization
+// 5. maintaining state in async world
+// 6. setTimeouts
+// 7. Iterators
+// 8. Many more patterns
+
+function z() {
+  var b = 900;
+  function x() {
+    var a = 7;
+    function y() {
+      console.log(a, b);
+    }
+    y();
   }
-  y();
+  x();
 }
-x();
+z();
