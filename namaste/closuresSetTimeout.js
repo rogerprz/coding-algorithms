03958c787dc8b75fb5ed6c1b4812ff177bacf8fa
Roger Perez <ropere@microsoft.com>
2024-06-16 22:48:01 -0700

    setTimeout

---
+++ namaste/closuresSetTimeout.js
@@ -0,0 +1,14 @@
+function x() {
+  for (var i = 1; i <= 5; i++) {
+    function close(i) {
+      setTimeout(function () {
+        console.log(i);
+      }, i * 1000);
+    }
+    close(i);
+  }
+
+  console.log('Hello');
+}
+x();
+// Output:
