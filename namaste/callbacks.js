1a9ae862262164d7d3ed7a4e2107df8766d67776
Roger Perez <ropere@microsoft.com>
2024-06-24 12:11:00 -0700

    callbacks

--- namaste/callbacks.js
+++ namaste/callbacks.js
@@ -1,5 +1,23 @@
 // What is a callback function in JavaScript?
+setTimeout(() => {
+  console.log('Hello');
+}, 1000);
+function x(y) {
+  console.log('x');
+  y();
+}
+
+x(function y() {
+  console.log('Hello');
+});
+//  Javascript is a synchronous, single-threaded language. This means that it can execute only one task at a time.
 
 // Blocking the main thread
 
 // Power of callbacks
+
+// Dive deep on event listeners
+
+// Scope demo with event listeners
+
+// Garbage collection & removeEventListener
