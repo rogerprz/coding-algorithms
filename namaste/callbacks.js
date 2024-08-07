1018d0b3c4a5a96c9615a24aa270b50f30e7d97a
Roger Perez <ropere@microsoft.com>
2024-08-07 09:08:25 -0700

    add new code

--- namaste/callbacks.js
+++ namaste/callbacks.js
@@ -1,19 +1,22 @@
 // What is a callback function in JavaScript?
-setTimeout(() => {
-  console.log('Hello');
-}, 1000);
+setTimeout(function () {
+  console.log('timer');
+}, 5000);
+
 function x(y) {
   console.log('x');
   y();
 }
 
 x(function y() {
-  console.log('Hello');
+  console.log('y');
 });
 //  Javascript is a synchronous, single-threaded language. This means that it can execute only one task at a time.
 
 // Blocking the main thread
-
+// The call stack is the main thread.
+// Whatever is executed in the page is part of the call stack
+// We should never block or call stack, we need to use async operations to avoid blocking the call stack
 // Power of callbacks
 
 // Dive deep on event listeners
@@ -21,3 +24,17 @@ x(function y() {
 // Scope demo with event listeners
 
 // Garbage collection & removeEventListener
+// let count = 0;
+
+// const isId = document.getElementById('clickMe');
+// document.getElementById('clickMe').addEventListener('click', () => {
+//   console.log('clicked', ++count);
+// });
+
+function attachEventListener() {
+  let count = 0;
+  document.getElementById('clickMe').addEventListener('click', function xyz() {
+    console.log('Attach:', ++count);
+  });
+}
+attachEventListener();
