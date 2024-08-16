8fa1e8b434097cbfc8e45f70c16c84a18c4a08de
Roger Perez <ropere@microsoft.com>
2024-08-15 21:30:35 -0700

    typo

--- asyncCallbacksPromises/debounce.js
+++ asyncCallbacksPromises/debounce.js
@@ -6,7 +6,7 @@ function debounce(func, wait) {
   let timeout;
   return (...args) => {
     clearTimeout(timeout);
-    timeout = setTimeout(() => func(...arg), wait);
+    timeout = setTimeout(() => func(...args), wait);
   };
 }
 
